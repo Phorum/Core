585adb85231e1809abe815d8af4808d79f190ca7
ts77 <ts77@3247d6aa-85f2-0310-9c31-93178ea70538>
2005-07-19 17:56:04 +0000

    # corrected initial real_name custom profile

--- include/db/mysql.php
+++ include/db/mysql.php
@@ -3365,7 +3365,7 @@ function phorum_db_create_tables()
             "use_cookies" => "1",
             "use_bcc" => "1",
             "internal_version" => "" . PHORUMINTERNAL . "",
-            "PROFILE_FIELDS" => array('name'=>"real_name",'length'=> 255, 'html_disabled'=>1),
+            "PROFILE_FIELDS" => array(array('name'=>"real_name",'length'=> 255, 'html_disabled'=>1)),
             "enable_pm" => "1",
             "user_edit_timelimit" => "0",
             "enable_new_pm_count" => "1",
