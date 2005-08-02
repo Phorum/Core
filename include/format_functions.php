a8756f48497ee110dcf82fcc2994fb018960a1fc
ts77 <ts77@3247d6aa-85f2-0310-9c31-93178ea70538>
2005-08-02 17:41:48 +0000

    # don't strip whitespace only in the beginning of code-tags

--- include/format_functions.php
+++ include/format_functions.php
@@ -5,13 +5,13 @@ if ( !defined( "PHORUM" ) ) return;
 function phorum_format_messages( $data )
 {
     $PHORUM = $GLOBALS["PHORUM"];
-    
+
     // retrieving bad-words list
     $banlists=phorum_db_get_banlists();
     if(isset($banlists[PHORUM_BAD_WORDS])) {
         $bad_words=$banlists[PHORUM_BAD_WORDS];
     } else {
-        $bad_words=array();   
+        $bad_words=array();
     }
 
     foreach( $data as $key => $message ) {
@@ -20,7 +20,7 @@ function phorum_format_messages( $data )
         $replace_vals  = array();
         $replace_words = array();
         $bad_word_check= false;
-        
+
         if ( is_array( $bad_words ) ) {
             foreach( $bad_words as $item ) {
                 $replace_words[]="/\b".$item['string']."(ing|ed|s|er|es)*\b/";
@@ -29,38 +29,38 @@ function phorum_format_messages( $data )
                 $bad_word_check=true;
             }
         }
-        
+
         // Work on the body
-        
+
         if ( isset( $message["body"] ) ) {
             $body = $message["body"];
-            
+
             // convert legacy <> urls into bare urls
             $body = preg_replace("/<((http|https|ftp):\/\/[a-z0-9;\/\?:@=\&\$\-_\.\+!*'\(\),~%]+?)>/i", "$1", $body);
-            
+
             // htmlspecialchars does too much
-            $body = str_replace( array( "&", "<", ">" ), array( "&amp;", "&lt;", "&gt;" ), $body ); 
-            
+            $body = str_replace( array( "&", "<", ">" ), array( "&amp;", "&lt;", "&gt;" ), $body );
+
             // replace newlines with <br phorum="true" /> temporarily
             // this way the mods know what Phorum did vs the user
             $body = str_replace( "\n", "<br phorum=\"true\" />\n", $body );
-            
+
             if($bad_word_check) {
                    $body = preg_replace( $replace_words, $replace_vals, $body );
             }
 
             $data[$key]["body"] = $body;
-        } 
+        }
         // ////////////////////////////////
-        
+
         // Work on the other fields
-        
-        
+
+
         // bad words on subject and author
         if($bad_word_check) {
              list($message["subject"],$message["author"]) = preg_replace( $replace_words, $replace_vals, array($message["subject"],$message["author"]));
         }
-        
+
         // htmlspecialchars does too much
         $safe_author = str_replace( array( "<", ">" ), array( "&lt;", "&gt;" ), $message["author"] );
         if($safe_author!=$data[$key]["author"]){
@@ -70,17 +70,17 @@ function phorum_format_messages( $data )
             }
             $data[$key]["author"] = $safe_author;
         }
-        
+
         $data[$key]["author"] = str_replace( array( "<", ">" ), array( "&lt;", "&gt;" ), $message["author"] );
 
         $data[$key]["email"] = str_replace( array( "<", ">" ), array( "&lt;", "&gt;" ), $message["email"] );
 
         $data[$key]["subject"] = str_replace( array( "&", "<", ">" ), array( "&amp;", "&lt;", "&gt;" ), $message["subject"] );
-    } 
+    }
     // run message formatting mods
     $data = phorum_hook( "format", $data );
 
-    $nobr_tags = array( "pre", "xmp" ); 
+    $nobr_tags = array( "pre", "xmp" );
     // clean up after the mods are done.
     foreach( $data as $key => $message ) {
         if ( isset( $message["body"] ) ) {
@@ -91,18 +91,18 @@ function phorum_format_messages( $data )
                         $stripped = str_replace( "<br phorum=\"true\" />", "", $match );
 
                         $message["body"] = str_replace( $match, $stripped, $message["body"] );
-                    } 
-                } 
+                    }
+                }
                 // fiddle with white space around quote and code tags.
-                $message["body"] = preg_replace( "/\s*(<\/*(xmp|blockquote|pre).*?>)\s*/", "$1", $message["body"] );
-            } 
+                $message["body"] = preg_replace( "/\s*(<\/*(xmp|blockquote|pre).*?>)(\s*\\n)/", "$1", $message["body"] );
+            }
             // normalize the <br /> tags
             $data[$key]["body"] = str_replace( "<br phorum=\"true\" />", "<br />", $message["body"] );
-        } 
-    } 
+        }
+    }
 
     return $data;
-} 
+}
 
 function phorum_date( $picture, $ts )
 {
@@ -110,8 +110,8 @@ function phorum_date( $picture, $ts )
     // setting locale
     if(!isset($PHORUM['locale']))
         $PHORUM['locale']="EN";
-        
-    setlocale(LC_TIME, $PHORUM['locale']); 
+
+    setlocale(LC_TIME, $PHORUM['locale']);
 
     if($PHORUM["user_time_zone"] && isset( $PHORUM["user"]["tz_offset"] ) && $PHORUM["user"]["tz_offset"]!=-99 ){
 
@@ -125,16 +125,16 @@ function phorum_date( $picture, $ts )
 
     }
 
-} 
+}
 
 function phorum_strip_body( $body )
-{ 
+{
     // strip HTML
-    $body = preg_replace( "|</*[a-z][^>]*>|i", "", $body ); 
+    $body = preg_replace( "|</*[a-z][^>]*>|i", "", $body );
     // strip BB Code
     $body = preg_replace( "|\[/*[a-z][^\]]*\]|i", "", $body );
 
     return $body;
-} 
+}
 
 ?>
