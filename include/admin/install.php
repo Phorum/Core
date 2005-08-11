faa9acc0d099c35ec08775a8f2b2c7c23f4a4a11
brian <brian@3247d6aa-85f2-0310-9c31-93178ea70538>
2005-08-11 03:28:23 +0000

    # new install checks to be sure we don't allow anyone at the install screen that shouldn't be.  Also fixes bug #73

--- include/admin/install.php
+++ include/admin/install.php
@@ -49,11 +49,11 @@
                         if(!empty($_SERVER["HTTP_REFERER"])) {
                             $http_path=$_SERVER["HTTP_REFERER"];
                         } elseif(!empty($_SERVER['HTTP_HOST'])) {
-                            $http_path="http://".$_SERVER['HTTP_HOST'];                        
+                            $http_path="http://".$_SERVER['HTTP_HOST'];
                             $http_path.=$_SERVER['PHP_SELF'];
                         } else {
                             $http_path="http://".$_SERVER['SERVER_NAME'];
-                            $http_path.=$_SERVER['PHP_SELF'];                            
+                            $http_path.=$_SERVER['PHP_SELF'];
                         }
                         phorum_db_update_settings(array("http_path"=>dirname($http_path)));
                         phorum_db_update_settings(array("system_email_from_address"=>$_POST["admin_email"]));
@@ -79,11 +79,12 @@
             $frm =& new PhorumInputForm ("", "post", "Continue ->");
             $frm->addbreak("Welcome to Phorum");
             $frm->addmessage("This wizard will setup Phorum on your server.  The first step is to prepare the database.  Phorum has already confirmed that it can connect to your database.  Press continue when you are ready.");
+            $frm->hidden("module", "install");
             $frm->hidden("step", "1");
             $frm->show();
 
             break;
-            
+
         case 1:
             if(isset($PHORUM['internal_version']) && $PHORUM['internal_version'] < PHORUMINTERNAL) {
                 $message="Phorum detected that you are running an old version of itself.<br />It will now try to upgrade the tables to the latest version.";
@@ -92,13 +93,14 @@
                 $message="Phorum detected you don't have any tables or another problem on searching the tables.<br />It will now try to create the tables.";
                 $setstep=2;
             }
-            
+
             $frm =& new PhorumInputForm ("", "post", "Continue ->");
             $frm->addbreak("Checking for upgrade ....");
             $frm->addmessage($message);
+            $frm->hidden("module", "install");
             $frm->hidden("step", $setstep);
-            $frm->show();     
-            
+            $frm->show();
+
             break;
 
         case 2:
@@ -112,21 +114,22 @@
             } else {
                 $message="Tables created.  Next we will check your cache settings. Press continue when ready.";
             }
-            
+
             $frm =& new PhorumInputForm ("", "post", "Continue ->");
             $frm->addbreak("Creating tables....");
             $frm->addmessage($message);
             $frm->hidden("step", "6");
+            $frm->hidden("module", "install");
             $frm->show();
 
             break;
-            
+
         case 3:
             // ok upgrading tables
             $message = phorum_upgrade_tables($PHORUM['internal_version'],PHORUMINTERNAL);
-            
+
             $frm =& new PhorumInputForm ("", "post", "Continue ->");
-            
+
             // done or not done? ;)
             $stepsleft = PHORUMINTERNAL - $PHORUM['internal_version'];
             $frm->addbreak("Upgrading tables (multiple steps possible) ....");
@@ -137,16 +140,18 @@
                 $newstep = 5;
             }
             $frm->hidden("step", $newstep);
-            $frm->show();           
-            
+            $frm->hidden("module", "install");
+            $frm->show();
+
             break;
 
         case 4:
 
             $frm =& new PhorumInputForm ("", "post");
+            $frm->hidden("step", "5");
+            $frm->hidden("module", "install");
             $frm->addbreak("Creating An Administrator");
             $frm->addmessage("Please enter the following information.  This can be your user information or you can create an administrator that is separate from yourself.<br /><br />Note: If you are using a pre-existing authentication database, please enter the username and password of the admin user that already exists.");
-            $frm->hidden("step", "5");
             $frm->addrow("Admin User Name", $frm->text_box("admin_user", "", 30));
             $frm->addrow("Admin Email Address", $frm->text_box("admin_email", "", 30));
             $frm->addrow("Admin Password", $frm->text_box("admin_pass", "", 30, 0, true));
@@ -157,6 +162,7 @@
 
         case 5:
 
+            phorum_db_update_settings( array("installed"=>1) );
             echo "The setup is complete.  You can now go to <a href=\"$_SERVER[PHP_SELF]\">the admin</a> and start making Phorum all your own.<br /><br /><strong>Here are some things you will want to look at:</strong><br /><br /><a href=\"$_SERVER[PHP_SELF]?module=settings\">The General Settings page</a><br /><br /><a href=\"docs/faq.txt\">The FAQ</a><br /><br /><a href=\"docs/performance.txt\">How to get peak performance from Phorum</a><br /><br /><strong>For developers:</strong><br /><br /><a href=\"docs/creating_mods.txt\">Module Creation</a><br /><br /><a href=\"docs/permissions.txt\">How Phorum permisssions work</a><br /><br /><a href=\"docs/CODING-STANDARDS\">The Phorum Team's codings standards</a>";
 
             break;
@@ -191,8 +197,9 @@
             } else {
                 $message.="Cache directory set.  Next we will create a user with administrator privileges.  Press continue when ready.";
             }
-            
+
             $frm =& new PhorumInputForm ("", "post", "Continue ->");
+            $frm->hidden("module", "install");
             $frm->addbreak("Checking cache....");
             $frm->addmessage($message);
             $frm->hidden("step", "4");
