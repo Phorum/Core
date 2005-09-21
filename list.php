c8acc460928d6e66d0bce7956b124bada5592c39
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2005-09-21 11:02:47 +0000

    # Fixed #70

--- list.php
+++ list.php
@@ -105,8 +105,16 @@ if($PHORUM['threaded_list']) { // make it simpler :)
     $PHORUM["list_length"] = $PHORUM['list_length_flat'];
 }
 
+// See how many sticky messages we have. These should be
+// kept out of the thread count for determining the 
+// number of pages to show.
+$sticky_count = 0;
+foreach ($rows as $row) {
+    if ($row["sort"] == PHORUM_SORT_STICKY) $sticky_count ++;
+}
+
 // figure out paging for threaded and flat mode
-$pages=ceil($PHORUM["thread_count"]/$PHORUM["list_length"]);
+$pages=ceil(($PHORUM["thread_count"] - $sticky_count) / $PHORUM["list_length"]);
 
 if($pages<=11){
     $page_start=1;
