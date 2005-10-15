532dd230da3f61617d674b4377735ac3dfc49448
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2005-10-15 02:20:41 +0000

    # Implemented phorum_check_bans() into the editor code

--- include/posting/check_banlist.php
+++ include/posting/check_banlist.php
@@ -22,50 +22,29 @@ if(!defined("PHORUM")) return;
 // For phorum_check_ban_lists().
 include_once("./include/profile_functions.php");
 
-// A mapping from bantype -> errormessage.
-$bantype2error = array(
-    PHORUM_BAD_NAMES  => "ErrBannedName",
-    PHORUM_BAD_EMAILS => "ErrBannedEmail",
-    PHORUM_BAD_USERID => "ErrBannedUser",
-    PHORUM_BAD_IPS    => "ErrBannedIP",
-);
-
 // Create a list of the bans that we want to check.
 $bans = array();
 
-if ($PHORUM["DATA"]["LOGGEDIN"]) { // Checks for registered users.
+// Add checks for registered users.
+if ($PHORUM["DATA"]["LOGGEDIN"]) {
     $bans[] = array($PHORUM["user"]["username"], PHORUM_BAD_NAMES);
     $bans[] = array($PHORUM["user"]["email"], PHORUM_BAD_EMAILS);
     $bans[] = array($PHORUM["user"]["user_id"], PHORUM_BAD_USERID);
-} else { // Checks for unregistered users.
+}
+// Add checks for unregistered users.
+else {
     $bans[] = array($message["author"], PHORUM_BAD_NAMES);
     $bans[] = array($message["email"], PHORUM_BAD_EMAILS);
 }
 
-// Check the IP-address for blacklisting. Also check the hostname
-// for blacklisting if dns_lookup was enabled.
-$bans[] = array($_SERVER["REMOTE_ADDR"], PHORUM_BAD_IPS, "ErrBannedIP");
-if ($PHORUM["dns_lookup"]) {
-    $REMOTE_ADDR = @gethostbyaddr($_SERVER["REMOTE_ADDR"]);
-    if ($REMOTE_ADDR != $_SERVER["REMOTE_ADDR"])
-        $bans[] = array($REMOTE_ADDR, PHORUM_BAD_IPS, "ErrBannedIP");
-} else {
-    $REMOTE_ADDR = $_SERVER["REMOTE_ADDR"];
-}
-
-// Load the ban lists
-$PHORUM["banlists"] = phorum_db_get_banlists();
+// Add check for IP-address bans.
+$bans[] = array(NULL, PHORUM_BAD_IPS);
 
 // Run the checks.
-foreach ($bans as $ban)
-{
-    if (! phorum_check_ban_lists($ban[0], $ban[1]))
-    {
-        $msg = $PHORUM["DATA"]["LANG"][$bantype2error[$ban[1]]];
-        $PHORUM["DATA"]["MESSAGE"] = $msg;
-        $error_flag = true;
-        break;
-    }
+$msg = phorum_check_bans($bans);
+if (!is_null($msg)) {
+    $PHORUM["DATA"]["MESSAGE"] = $msg;
+    $error_flag = true;
 }
 
 ?>
