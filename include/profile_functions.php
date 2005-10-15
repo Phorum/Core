bb26c55c5df2f431314c172025a2d4b61f8af6f7
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2005-10-15 02:19:54 +0000

    Added phorum_check_bans() for easy checking and fixed phorum_check_ban_lists().

--- include/profile_functions.php
+++ include/profile_functions.php
@@ -46,9 +46,89 @@ function phorum_gen_password($charpart=4, $numpart=3)
     return strtolower($password.$num);
 }
 
+// ----------------------------------------------------------------------------
+// Banlist checking
+// ----------------------------------------------------------------------------
+
+/**
+ * This function can perform multiple banlist checks at once and will
+ * automatically generate an appropriate error message when a banlist
+ * match is found.
+ * @param bans - an array of bans to check. Each element in this array is an
+ *               array itself with two elements: the value to check and the 
+ *               type of banlist to check against. One special case:
+ *               if the type if PHORUM_BAD_IPS, the value may be NULL.
+ *               In that case the IP/hostname of the client will be checked.
+ * @return - An error message in case a banlist match was found or NULL
+ *           if no match was found.
+ */
+function phorum_check_bans($bans)
+{
+    $PHORUM = $GLOBALS["PHORUM"];
+
+    // A mapping from bantype -> error message to return on match.
+    $phorum_bantype2error = array (
+        PHORUM_BAD_NAMES  => "ErrBannedName",
+        PHORUM_BAD_EMAILS => "ErrBannedEmail",
+        PHORUM_BAD_USERID => "ErrBannedUser",
+        PHORUM_BAD_IPS    => "ErrBannedIP",
+    );
+
+    // Load the ban lists.
+    if (! isset($GLOBALS["PHORUM"]["banlists"]))
+        $GLOBALS["PHORUM"]["banlists"] = phorum_db_get_banlists();
+    if(! isset($GLOBALS['PHORUM']['banlists'])) return NULL;
+
+    // Run the checks.
+    for (;;) {
+        // An array for adding ban checks on the fly.
+        $add_bans = array();
+
+        foreach ($bans as $ban) {
+            // Checking IP/hostname, but no value set? Then add the IP-address
+            // and hostname (if DNS lookups are enabled) to the end of the checking
+            // queue and continue with the next check.
+            if ($ban[1] == PHORUM_BAD_IPS && $ban[0] == NULL) {
+                $add_bans[] = array($_SERVER["REMOTE_ADDR"], PHORUM_BAD_IPS);
+                if ($PHORUM["dns_lookup"]) {
+                    $resolved = @gethostbyaddr($_SERVER["REMOTE_ADDR"]);
+                    if (!empty($resolved) && $resolved != $_SERVER["REMOTE_ADDR"]) {
+                        $add_bans[] = array($resolved, PHORUM_BAD_IPS);
+                    }
+                }
+                continue;
+            }
+
+            // Do a single banlist check. Return an error if we find a match.
+            if (! phorum_check_ban_lists($ban[0], $ban[1])) {
+                $msg = $PHORUM["DATA"]["LANG"][$phorum_bantype2error[$ban[1]]];
+                return $msg;
+            }
+        }
+
+        // Bans added on the fly? Then restart the loop.
+        if (count($add_bans) == 0) {
+            break;
+        } else {
+            $bans = $add_bans;
+        }
+    }
+
+    return NULL;
+}
+
+/**
+ * Check a single banlist for a match.
+ * @param value - The value to check.
+ * @param type - The type of banlist to check the value against.
+ * @return True if all is okay. False if a match has been found.
+ */
 function phorum_check_ban_lists($value, $type)
 {
-    if(!isset($GLOBALS['PHORUM']['banlists'])) return true;
+    // Load the ban lists.
+    if (! isset($GLOBALS["PHORUM"]["banlists"]))
+        $GLOBALS["PHORUM"]["banlists"] = phorum_db_get_banlists();
+    if(! isset($GLOBALS['PHORUM']['banlists'])) return true;
 
     $banlists = $GLOBALS['PHORUM']['banlists'];
 
@@ -57,9 +137,10 @@ function phorum_check_ban_lists($value, $type)
     if (!empty($value)) {
         if (isset($banlists[$type]) && is_array($banlists[$type])) {
             foreach($banlists[$type] as $item) {
-                if (!empty($item['item']) && (($item["pcre"] && preg_match("/\b".preg_quote($item[string])."\b/i", $value)) ||
-                        (!$item["pcre"] && stristr($value , $item["string"]) && $type != PHORUM_BAD_USERID) ||
-                        ($type == PHORUM_BAD_USERID && $value == $item["string"]))) {
+                if ( !empty($item['string']) && (
+                     ($item["pcre"] && preg_match("/\b".$item['string']."\b/i", $value)) ||
+                     (!$item["pcre"] && stristr($value , $item["string"]) && $type != PHORUM_BAD_USERID) ||
+                     ($type == PHORUM_BAD_USERID && $value == $item["string"])) ) {
                     return false;
                 }
             }
