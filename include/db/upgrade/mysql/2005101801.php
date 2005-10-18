6e93a4dc62f320e4d8a6000fce20954277f9fe3d
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2005-10-18 13:48:17 +0000

    # Fix in db upgrade script for [573]

--- include/db/upgrade/mysql/2005101801.php
+++ include/db/upgrade/mysql/2005101801.php
@@ -4,7 +4,7 @@ if(!defined("PHORUM_ADMIN")) return;
 // Refresh all forum statistics for making bringing thread_count up to date.
 $forums = phorum_db_get_forums();
 foreach ($forums as $forum) {
-    $PHORUM["forum_id"] = $forum["forum_id"];
+    $GLOBALS["PHORUM"]["forum_id"] = $forum["forum_id"];
     phorum_db_update_forum_stats(true);
 }
 ?>
