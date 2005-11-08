5c23e120b50fb5f7686c10db4fafefea26aa6b38
ts77 <ts77@3247d6aa-85f2-0310-9c31-93178ea70538>
2005-11-08 21:48:20 +0000

    "fixed removing newlines in table-definitions (kudos to brian)"

--- mods/html/html.php
+++ mods/html/html.php
@@ -7,15 +7,15 @@ function phorum_html($data)
 {
 	$PHORUM = $GLOBALS["PHORUM"];
 
-	foreach($data as $message_id => $message){ 
+	foreach($data as $message_id => $message){
 
         if(isset($message["body"])){
 
-            $body = $message["body"]; 
-    
+            $body = $message["body"];
+
             // restore tags where Phorum has killed them
             $body = preg_replace("!&lt;(\/*[a-z].*?)&gt;!i", "<$1>", $body);
-    
+
             // restore escaped &
             $body = str_replace("&amp;", "&", $body);
 
@@ -27,20 +27,20 @@ function phorum_html($data)
                     $body=str_replace($tag, $newtag, $body);
                 }
             }
-    
+
             // turn script and meta tags into comments
             $body=preg_replace("/<(\/*(script|meta).*?)>/i", "<!--$1-->", $body);
-     
-            // strip any <br phorum=\"true\" /> that got inside certain blocks like tables and pre.         
+
+            // strip any <br phorum=\"true\" /> that got inside certain blocks like tables and pre.
             $block_tags="table|pre|xmp";
-    
-            preg_match_all("!(<($block_tags).*?>).+?(</($block_tags).*?>)!", $body, $matches);
-            
+
+            preg_match_all("!(<($block_tags).*?>).+?(</($block_tags).*?>)!ms", $body, $matches);
+
             foreach($matches[0] as $block){
                 $newblock=str_replace("<br phorum=\"true\" />", "", $block);
                 $body=str_replace($block, $newblock, $body);
             }
-           
+
     		$data[$message_id]["body"] = $body;
         }
 	}
