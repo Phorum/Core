0ceef2553af4797f9919f71e2f2e90ae26dfeaa1
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2006-01-06 15:39:38 +0000

    # Updated copyright notices to 2006

--- include/admin/newforum.php
+++ include/admin/newforum.php
@@ -2,7 +2,7 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 //                                                                            //
-//   Copyright (C) 2005  Phorum Development Team                              //
+//   Copyright (C) 2006  Phorum Development Team                              //
 //   http://www.phorum.org                                                    //
 //                                                                            //
 //   This program is free software. You can redistribute it and/or modify     //
