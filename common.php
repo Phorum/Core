46ba63ec5156ee5375d3227a2b3a3614ccb60ea5
brian <brian@3247d6aa-85f2-0310-9c31-93178ea70538>
2006-01-26 06:53:50 +0000

    # bumped version

--- common.php
+++ common.php
@@ -21,7 +21,7 @@
 if ( basename( __FILE__ ) == basename( $_SERVER["PHP_SELF"] ) ) exit();
 
 // all other constants in ./include/constants.php
-define( "PHORUM", "5.1.6a" );
+define( "PHORUM", "5.1.7-RC1" );
 
 // our internal version in format of year-month-day-serial
 define( "PHORUMINTERNAL", "2005120400" );
