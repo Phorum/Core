5f69c9f10f3cfcd1faea584a853ce3bc6b947cda
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2006-02-20 00:33:50 +0000

    # I think this change deserves a template version bump

--- templates/default/info.php
+++ templates/default/info.php
@@ -3,7 +3,7 @@
     if(!defined("PHORUM")) return;
 
     $name="Default Phorum Template";
-    $version="0.3";
+    $version="0.4";
     // uncomment this to hide this template from the user-select-box
     //$template_hide=1;
 
