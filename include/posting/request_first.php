bedeea9274a0eb586e8ba8c965617199afc5cf3b
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2006-05-12 13:52:40 +0000

    # Rollback of previous change due to unwanted side effects when quoting messages

--- include/posting/request_first.php
+++ include/posting/request_first.php
@@ -41,18 +41,12 @@ if ($mode != "post")
     // Check read access on the forum that we're handling.
     if (!phorum_check_read_common()) exit;
 
-    // If we have the message in the environment already, there's no need to
-    // load it from the database. So check if we can use the environment.
-    // If not, then we load the message from the database. If the message
+    // Load the message from the database. If the message
     // can't be retrieved, then return to the message list.
-    if (isset($PHORUM["DATA"]["MESSAGES"]) && isset($PHORUM["DATA"]["MESSAGES"][$message_id])) {
-        $dbmessage = $PHORUM["DATA"]["MESSAGES"][$message_id];
-    } else {
-        $dbmessage = phorum_db_get_message($message_id);
-        if (! $dbmessage) {
-            phorum_redirect_by_url(phorum_get_url(PHORUM_LIST_URL));
-            exit;
-        }
+    $dbmessage = phorum_db_get_message($message_id);
+    if (! $dbmessage) {
+        phorum_redirect_by_url(phorum_get_url(PHORUM_LIST_URL));
+        exit;
     }
 }
 
