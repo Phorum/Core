f415c49d6730e0ee4805745a8a9b7698fefae05c
ts77 <ts77@3247d6aa-85f2-0310-9c31-93178ea70538>
2006-06-02 14:54:42 +0000

    "catching error-condition in reporting, should fix #353"

--- report.php
+++ report.php
@@ -34,23 +34,26 @@ if(!phorum_check_read_common()) {
 $report = false;
 $template = "report";
 
+$message=array();
 // get the message
 if (is_numeric($PHORUM["args"][1])) {
     $message_id = $PHORUM["args"][1];
     $message = phorum_db_get_message($message_id);
 }
 
-// check for report requests
-if(!empty($_POST["report"])) {
-    if ($PHORUM["DATA"]["LOGGEDIN"]){
-        if (empty($_POST["explanation"])){
-            $_POST["explanation"] = "<" . $PHORUM["DATA"]["LANG"]["None"] . ">";
-        }
+if(is_array($message) && count($message)) {
 
-        $mail_users = phorum_user_get_moderators($PHORUM['forum_id'],false,true);
+    // check for report requests
+    if(!empty($_POST["report"])) {
+        if ($PHORUM["DATA"]["LOGGEDIN"]){
+            if (empty($_POST["explanation"])){
+                $_POST["explanation"] = "<" . $PHORUM["DATA"]["LANG"]["None"] . ">";
+            }
 
-        if(count($mail_users)){
-            $mail_data = array(
+            $mail_users = phorum_user_get_moderators($PHORUM['forum_id'],false,true);
+
+            if(count($mail_users)){
+                $mail_data = array(
                 "mailmessage" => $PHORUM["DATA"]["LANG"]['ReportPostEmailBody'],
                 "mailsubject" => $PHORUM["DATA"]["LANG"]['ReportPostEmailSubject'],
                 "forumname"   => $PHORUM["DATA"]["NAME"],
@@ -66,41 +69,44 @@ if(!empty($_POST["report"])) {
                 "hide_url"    => phorum_get_url(PHORUM_MODERATION_URL, PHORUM_HIDE_POST, $message_id),
                 "edit_url"    => phorum_get_url(PHORUM_POSTING_URL, 'moderation', $message_id),
                 "reporter_url"=> phorum_get_url(PHORUM_PROFILE_URL, $PHORUM["user"]["user_id"])
-            );
+                );
 
-            $mail_data = phorum_hook("report", $mail_data);
+                $mail_data = phorum_hook("report", $mail_data);
 
-            phorum_email_user($mail_users, $mail_data);
+                phorum_email_user($mail_users, $mail_data);
 
-        $PHORUM["DATA"]["URL"]["REDIRECT"]=phorum_get_url(PHORUM_FOREIGN_READ_URL, $message["forum_id"], $message["thread"]);
-        $PHORUM["DATA"]["BACKMSG"]=$PHORUM["DATA"]["LANG"]["BackToThread"];
-        $PHORUM["DATA"]["MESSAGE"]=$PHORUM["DATA"]["LANG"]["ReportPostSuccess"];
-        $template="message";
-            $report = true;
+                $PHORUM["DATA"]["URL"]["REDIRECT"]=phorum_get_url(PHORUM_FOREIGN_READ_URL, $message["forum_id"], $message["thread"]);
+                $PHORUM["DATA"]["BACKMSG"]=$PHORUM["DATA"]["LANG"]["BackToThread"];
+                $PHORUM["DATA"]["MESSAGE"]=$PHORUM["DATA"]["LANG"]["ReportPostSuccess"];
+                $template="message";
+                $report = true;
+            }
+        }
+        else{
+            $PHORUM["DATA"]["ReportPostMessage"] = $PHORUM["DATA"]["LANG"]['ReportPostNotAllowed'];
         }
     }
-    else{
-        $PHORUM["DATA"]["ReportPostMessage"] = $PHORUM["DATA"]["LANG"]['ReportPostNotAllowed'];
+
+    // format message
+    list($message) = phorum_format_messages(array($message));
+
+    $PHORUM["DATA"]["PostSubject"] = $message["subject"];
+    $PHORUM["DATA"]["PostAuthor"] = $message["author"];
+    $PHORUM["DATA"]["PostBody"] = $message["body"];
+    $PHORUM["DATA"]["PostDate"] = phorum_date($PHORUM["short_date"], $message["datestamp"]);
+    $PHORUM["DATA"]["ReportURL"] = phorum_get_url(PHORUM_REPORT_URL, $message_id);
+
+    // if the report was not successfully sent, keep whatever explanation they gave already
+    if (isset($_POST["explanation"]) && !$report) {
+        $PHORUM["DATA"]["explanation"] = $_POST["explanation"];
     }
+    else {
+        $PHORUM["DATA"]["explanation"] = "";
+    }
+} else {
+
+    $PHORUM["DATA"]["ReportPostMessage"] = $PHORUM['DATA']['LANG'];
 }
-
-// format message
-list($message) = phorum_format_messages(array($message));
-
-$PHORUM["DATA"]["PostSubject"] = $message["subject"];
-$PHORUM["DATA"]["PostAuthor"] = $message["author"];
-$PHORUM["DATA"]["PostBody"] = $message["body"];
-$PHORUM["DATA"]["PostDate"] = phorum_date($PHORUM["short_date"], $message["datestamp"]);
-$PHORUM["DATA"]["ReportURL"] = phorum_get_url(PHORUM_REPORT_URL, $message_id);
-
-// if the report was not successfully sent, keep whatever explanation they gave already
-if (isset($_POST["explanation"]) && !$report) {
-    $PHORUM["DATA"]["explanation"] = $_POST["explanation"];
-}
-else {
-    $PHORUM["DATA"]["explanation"] = "";
-}
-
 include phorum_get_template("header");
 phorum_hook("after_header");
 include phorum_get_template($template);
