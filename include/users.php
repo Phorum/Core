d9ca4af194b6f738185a36c8fd9b25abbe5b44e4
ts77 <ts77@3247d6aa-85f2-0310-9c31-93178ea70538>
2006-06-14 23:22:40 +0000

    "# MFH: new hook for using a mod for authentication and synchronization"

--- include/users.php
+++ include/users.php
@@ -371,11 +371,20 @@ function phorum_user_check_login( $username, $password )
     $ret = false;
     $temp_check = false;
 
-    $user_id = phorum_db_user_check_pass( $username, md5( $password ) );
-    // regular password failed, try the temp password
-    if ( $user_id == 0 ) {
-        $user_id = phorum_db_user_check_pass( $username, md5( $password ), true );
-        $temp_check = true;
+    $temp_user =phorum_hook("user_check_login",array("user_id"=>"","username"=>$username,"password"=>$password));
+    if( is_array($temp_user) && !empty($temp_user["user_id"]) ) {
+        $user_id = $temp_user["user_id"];
+    }
+
+    if( !isset($user_id) || $user_id===false ) {
+
+        $user_id = phorum_db_user_check_pass( $username, md5( $password ) );
+        // regular password failed, try the temp password
+        if ( $user_id == 0 ) {
+            $user_id = phorum_db_user_check_pass( $username, md5( $password ), true );
+            $temp_check = true;
+        }
+
     }
 
     if ( $user_id > 0 ) {
@@ -391,9 +400,9 @@ function phorum_user_check_login( $username, $password )
         $ret = phorum_user_set_current_user( $user_id );
     }
 
+
     return $ret;
 }
-
 function phorum_user_verify( $user_id, $tmp_pass )
 {
     $user_id = phorum_db_user_check_field( array( "user_id", "password_temp" ), array( $user_id, md5( $tmp_pass ) ), array( "=", "=" ) );
