735f3ae76f544eb4b5840e516e0f0691d4e2f630
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2006-07-24 17:50:46 +0000

    # Added Spanish translation to the Editor Tools mod

---
+++ mods/editor_tools/help/spanish/smileys.php
@@ -0,0 +1,38 @@
+<?php
+  // The definition of the possible uses for a smiley.
+  $PHORUM_MOD_SMILEY_USES = array(
+      0   => "B",
+      1   => "S",
+      2   => "BS",
+  );
+?>
+<html>
+  <head>
+    <title>Ayuda Smileys</title>
+    <link rel="stylesheet" type="text/css" href="<?php print $GLOBALS["PHORUM"]["http_path"] ?>/mods/editor_tools/help/help.css"/>
+  </head>
+  <body>
+    <h2>Ayuda para usar los smileys</h2>
+
+	En este foro puedes utilizar smileys.
+	Los smileys son cadenas de caracteres que aparecerán como
+	una imagen mientras leas el mensaje. Los smileys suelen ser 
+	usados para expresar sentimiento en el texto. En la tabla inferior
+	puedes ver todos los smileys que están disponibles. La columna 
+	"Dónde" indica dónde pueden ser utilizados los smileys (S = Asunto, 
+	B = Cuerpo del mensaje, BS = ambos). 
+	
+	<br/><br/>
+    <table cellspacing="1" width="100%">
+    <tr>
+      <th class="PhorumAdminTableHead">Smiley</th>
+      <th class="PhorumAdminTableHead">Imagen</th>
+      <th class="PhorumAdminTableHead">Descripción</th>
+      <th class="PhorumAdminTableHead">Dónde</th>
+    </tr>
+    <?php include("./mods/editor_tools/help/render_smileys_list.php") ?>
+    </table>
+
+    <br/><br/><br/>
+  </body>
+</html>
---
+++ mods/editor_tools/lang/spanish.php
@@ -0,0 +1,43 @@
+<?php
+$PHORUM["DATA"]["LANG"]["mod_editor_tools"] = array(
+    # Button descriptions.
+    'bold'          => 'Negrita',
+    'italic'        => 'Cursiva',
+    'underline'     => 'Subrayado',
+    'strike'        => 'Tachado',
+    'subscript'     => 'Subindice',
+    'superscript'   => 'Superindice',
+    'color'         => 'Selecciona color de la fuente',
+    'size'          => 'Selecciona tamaño de la fuente',
+    'center'        => 'Centrar texto',
+    'url'           => 'Insertar URL',
+    'email'         => 'Insertar direccion de correo',
+    'image'         => 'Insertar URL de la imagen',
+    'smiley'        => 'Insertar smiley',
+    'subjectsmiley' => 'Insertar smiley en el asunto',
+    'hr'            => 'Insertar linea horizontal',
+    'code'          => 'Insertar texto de código',
+    'quote'         => 'Insertar cita textual',
+    'help'          => 'Ayuda',
+
+    # Dialogs and errors.
+    'enter url'     => 'Introduce la dirección URL a la que quieres crear un enlace.',
+    'invalid url'   => 'La URL es incorrecta. Debería comenzar con  with http://, https://, ftp:// o mailto:',
+    'enter image url' => 'Introduce la dirección URL de la imagen que deseas añadir.',
+    'invalid image url' => 'La URL de la imagen es incorrecta. Debería comenzar con http://, https:// o ftp://',
+    'enter who you quote' => 'Introduce el nombre de la persona que estás citando. Puedes omitirlo si lo prefieres.',
+
+    # For the size picker.
+    'x-large'      => 'Fuente Super Grande',
+    'large'        => 'Fuente Grande',
+    'medium'       => 'Fuente Normal',
+    'small'        => 'Fuente Pequeña',
+    'x-small'      => 'Fuente Super Pequeña',
+
+    # Help titles.
+    'bbcode help'  => 'Ayuda BBCode',
+    'smileys help' => 'Ayuda Smileys',
+
+    # For the non javascript help pages.
+    'help'         => 'Información de ayuda:',
+); ?>
