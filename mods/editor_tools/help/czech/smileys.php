3f65f365dbfe6d45f1710b0ac2c91dc923fc427e
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2006-07-27 19:25:41 +0000

    # Added Czech translation for the editor tools

---
+++ mods/editor_tools/help/czech/smileys.php
@@ -0,0 +1,33 @@
+<?php
+  // The definition of the possible uses for a smiley.
+  $PHORUM_MOD_SMILEY_USES = array(
+      0   => "B",
+      1   => "S",
+      2   => "BS",
+  );
+?>
+<html>
+  <head>
+    <title>Smajlící nápovìda</title>
+    <link rel="stylesheet" type="text/css" href="<?php print $GLOBALS["PHORUM"]["http_path"] ?>/mods/editor_tools/help/help.css"/>
+  </head>
+  <body>
+    <h2>Smajlící nápovìda</h2>
+
+		Na tomto fóru mùžete použít smajlíky. Smajlící jsou øetìzce znakù, které se ve zprávì zobrazují jako obrázky.
+		Ty se používají pro vyjádøení nálady autora pøíspìvku. V tabulce dole jsou všechny dostupné smajlíky. Ve sloupci "Kde"
+		je popis, kde je možné smajlík použít (S = subjekt zprávy, B = tìlo zprávy, BS = všude).	
+    <br/><br/>
+    <table cellspacing="1" width="100%">
+    <tr>
+      <th class="PhorumAdminTableHead">Smajlík</th>
+      <th class="PhorumAdminTableHead">Obrázek</th>
+      <th class="PhorumAdminTableHead">Popis</th>
+      <th class="PhorumAdminTableHead">Kde</th>
+    </tr>
+    <?php include("./mods/editor_tools/help/render_smileys_list.php") ?>
+    </table>
+
+    <br/><br/><br/>
+  </body>
+</html>
---
+++ mods/editor_tools/lang/czech.php
@@ -0,0 +1,44 @@
+<?php
+$PHORUM["DATA"]["LANG"]["mod_editor_tools"] = array(
+    # Button descriptions.
+    'bold'          => 'Bold',
+    'italic'        => 'Kurzíva',
+    'underline'     => 'Pdtrhnout',
+    'strike'        => 'Pøeškrtnout',
+    'subscript'     => 'Dolní index',
+    'superscript'   => 'Horní index',
+    'color'         => 'Barva písma',
+    'size'          => 'Velikost písma',
+    'center'        => 'Vycentrovat text',
+    'url'           => 'Vložit odkaz',
+    'email'         => 'Vložit emailovou adresu',
+    'image'         => 'Vložit odkaz obrázku',
+    'smiley'        => 'Vložit smajlíka',
+    'subjectsmiley' => 'Vložit smajlíka do subjektu',
+    'hr'            => 'Vložit øádku',
+    'code'          => 'Formátovaný kód',
+    'quote'         => 'Vložit kótovaný text',
+    'help'          => 'nápovìda',
+
+    # Dialogs and errors.
+    'enter url'     => 'Prosím, vložte adresu odkazu.',
+    'enter url description' => 'Prosím vložte popis odkazu.',
+    'invalid url'   => 'Odkaz je chybný. Mìl by zaèínat s http://, https://, ftp:// nebo mailto:',
+    'enter image url' => 'Prosím vložte odkaz na obrázek, který chcete pøidat.',
+    'invalid image url' => 'Chybný odkaz obrázku. Mìl by zaèínat s http://, https:// or ftp://',
+    'enter who you quote' => 'Prosím, vložte jméno osoby, která má být v uvozovkách. Mùžete také nechat prázdné.',
+
+    # For the size picker.
+    'x-large'      => 'Extra velké písmo',
+    'large'        => 'Velké písmo',
+    'medium'       => 'Støední písmo',
+    'small'        => 'Malé písmo',
+    'x-small'      => 'Extra malé písmo',
+
+    # Help titles.
+    'bbcode help'  => 'BBcode nápovìda',
+    'smileys help' => 'Smajlíky nápovìda',
+
+    # For the non javascript help pages.
+    'help'         => 'Informace nápovìdy:',
+); ?>
