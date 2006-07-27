98eff4a63f2424e7d8a1a97574531b61a2c82a1a
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2006-07-27 22:41:45 +0000

    # Added Finnish an Spanish update for the Editor Tools Module

--- mods/editor_tools/lang/finnish.php
+++ mods/editor_tools/lang/finnish.php
@@ -22,7 +22,7 @@ $PHORUM["DATA"]["LANG"]["mod_editor_tools"] = array(
 
     # Dialogs and errors.
     'enter url'     => 'Ole hyv‰ ja syˆt‰ linkin osoite.',
-    'enter url description' => 'Please, enter a description for the URL.',
+    'enter url description' => 'Ole hyv‰ ja syˆt‰ linkin kuvaus.',
     'invalid url'   => 'Osoite on ep‰kelpo. Sen pit‰‰ alkaa tekstill‰ http://, https://, ftp:// tai mailto:',
     'enter image url' => 'Ole hyv‰ ja syˆt‰ lis‰tt‰v‰n kuvan osoite.',
     'invalid image url' => 'Kuvan osoite on ep‰kelpo. Sen pit‰‰ alkaa tekstill‰ http://, https:// tai ftp://',
