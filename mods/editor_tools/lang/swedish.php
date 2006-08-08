c8ae995236a6750a9262e7f58fd9b57908b410be
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2006-08-08 11:54:39 +0000

    # Added Swedish translation to the editor tools module

---
+++ mods/editor_tools/lang/swedish.php
@@ -0,0 +1,44 @@
+﻿<?php
+$PHORUM["DATA"]["LANG"]["mod_editor_tools"] = array(
+    # Button descriptions.
+    'bold'          => 'Fet',
+    'italic'        => 'Kursiv',
+    'underline'     => 'Undertruket',
+    'strike'        => 'Genomstruket',
+    'subscript'     => 'Liten text nere (Subscript)',
+    'superscript'   => 'Liten text uppe (Superscript)',
+    'color'         => 'Välj teckenfärg',
+    'size'          => 'Välj teckenstorlek',
+    'center'        => 'Centrera texten',
+    'url'           => 'Infoga sökväg (URL)',
+    'email'         => 'Infoga e-post adress',
+    'image'         => 'Infoga sökväg (URL) till bild',
+    'smiley'        => 'Infoga smiley',
+    'subjectsmiley' => 'Infoga smiley till rubriken',
+    'hr'            => 'Infoga liggande linje',
+    'code'          => 'Formatterad kod',
+    'quote'         => 'Infoga citerad text',
+    'help'          => 'hjälp',
+
+    # Dialogs and errors.
+    'enter url'     => 'Var vänlig ange sökvägen, URLen, dit du vill skapa en länk.',
+    'enter url description' => 'Var vänlig ange en beskrivning för länken.',
+    'invalid url'   => 'Sökvägen är inte rätt skriven. Den ska ska börja med http://, https://, ftp:// eller mailto:',
+    'enter image url' => 'Var vänlig ange sökvägen, URLen, till den bild du vill lägga till.',
+    'invalid image url' => 'Sökvägen till bilden är inte rätt skriven. Den ska ska börja med http://, https:// eller ftp://',
+    'enter who you quote' => 'Var vänlig ange namnet pÂ den person du citerar. Du kan lämna det här tomt om du vill.',
+
+    # For the size picker.
+    'x-large'      => 'Extra stor textstorlek',
+    'large'        => 'Stor textstorlek',
+    'medium'       => 'Medium textstorlek',
+    'small'        => 'Liten textstorlek',
+    'x-small'      => 'Extra liten textstorlek',
+
+    # Help titles.
+    'bbcode help'  => 'BBcode hjälp',
+    'smileys help' => 'Smileys  hjälp',
+
+    # For the non javascript help pages.
+    'help'         => 'Hjälp information:',
+); ?>
