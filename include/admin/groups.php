db005312cc6b4777a8aa9473084f49999419a730
ts77 <ts77@3247d6aa-85f2-0310-9c31-93178ea70538>
2006-10-01 17:57:15 +0000

    MFB: # showing forums with their path in group-settings too

--- include/admin/groups.php
+++ include/admin/groups.php
@@ -24,14 +24,14 @@
     if(count($_POST)){
 
         if( isset($_POST['action']) && $_POST['action'] == "deleteGroups") {
-        
+
           $count=0;
           foreach($_POST['deleteIds'] as $id => $deluid) {
               phorum_db_delete_group($deluid);
               $count++;
           }
-          echo "$count Group(s) deleted.<br />";          
-          
+          echo "$count Group(s) deleted.<br />";
+
         }
 
         switch ($_POST["section"]) {
@@ -46,7 +46,7 @@
                     echo "Error adding group<br />";
                 } else {
                     echo "Group added<br />";
-                }        
+                }
                 break;
 
             case "edit":
@@ -58,7 +58,7 @@
                     echo "Error Saving Group Name";
                 }
                 break;
-        
+
             case "forums":
                 $group=$_POST;
                 if($_POST["new_forum"]){
@@ -85,7 +85,7 @@
                 if(isset($_POST["forums"])){
                     foreach($_POST["forums"] as $forum_id){
                         $permission=0;
-                            
+
                         if(isset($group["permissions"][$forum_id])){
                             foreach($group["permissions"][$forum_id] as $perm=>$check){
                                 $permission = $permission | $perm;
@@ -105,16 +105,16 @@
                 	// clearing user-cache if needed
                 	if(isset($PHORUM['cache_users']) && $PHORUM['cache_users']) {
 	                	$group_members=phorum_db_get_group_members($_POST["group_id"]);
-	                	
+
 	                	if(count($group_members)) {
 	                		foreach($group_members as $user_id => $user_status) {
-	                			phorum_cache_remove('user',$user_id);	
+	                			phorum_cache_remove('user',$user_id);
 	                		}
 	                	}
-	                	
+
                 	}
-                	
-                	
+
+
                     echo "Group Forum Permissions Saved";
                 } else {
                     echo "Error Saving Group Forum Permissions";
@@ -124,7 +124,7 @@
                 break;
 
         }
-        
+
     }
 
     if($error){
@@ -133,7 +133,7 @@
 
     include_once "./include/admin/PhorumInputForm.php";
     $groups=phorum_db_get_groups();
-   
+
     $forums=phorum_db_get_forums();
 
     if(isset($_GET["edit"])){
@@ -141,20 +141,20 @@
         $group=$groups[$_GET["group_id"]];
 
         $frm =& new PhorumInputForm ("", "post");
-    
+
         $frm->addbreak("Edit Group");
-    
+
         $frm->hidden("module", "groups");
-    
+
         $frm->hidden("section", "edit");
 
         $frm->hidden("group_id", $_GET["group_id"]);
-    
-        $open_options = array(PHORUM_GROUP_CLOSED => "No", 
+
+        $open_options = array(PHORUM_GROUP_CLOSED => "No",
                         PHORUM_GROUP_OPEN => "Yes",
                         PHORUM_GROUP_REQUIRE_APPROVAL => "Yes (require Group Moderator approval)");
         $frm->addrow("Name:", $frm->text_box("name", $group["name"], 50));
-        $frm->addrow("Allow Membership Requests:", $frm->select_tag("open", $open_options, $group["open"], ""));        
+        $frm->addrow("Allow Membership Requests:", $frm->select_tag("open", $open_options, $group["open"], ""));
         $frm->show();
 
         echo "<br /><hr class=\"PhorumAdminHR\" /><br />";
@@ -163,7 +163,7 @@
         $frm =& new PhorumInputForm ("", "post", "Update");
 
         $frm->hidden("module", "groups");
-    
+
         $frm->hidden("section", "forums");
 
         $frm->hidden("group_id", $_GET["group_id"]);
@@ -171,8 +171,8 @@
         $row=$frm->addbreak("Edit Forum Permissions");
 
         $frm->addhelp($row, "Forum Permissions", "Permissions given to groups overwrite any permissions granted by the forum properties.  Also, if a user is granted permissions directly to a forum in the user admin, any group permissions he has for that forum will be ignored.  If the user is a member of two or more groups that have permissions in the same forum, the permissions will be combined. (eg. If group A allows read and reply and group B allows create and moderate, the user will receive all four permissions.)");
-        
-        $forums=phorum_db_get_forums();
+
+        $forumpaths = phorum_get_forum_info(1);
 
         $perm_frm = $frm->checkbox("new_permissions[".PHORUM_USER_ALLOW_READ."]", 1, "Read")."&nbsp;&nbsp;".
                     $frm->checkbox("new_permissions[".PHORUM_USER_ALLOW_REPLY."]", 1, "Reply")."&nbsp;&nbsp;".
@@ -182,13 +182,13 @@
                     $frm->checkbox("new_permissions[".PHORUM_USER_ALLOW_MODERATE_MESSAGES."]", 1, "Moderate Messages")."&nbsp;&nbsp;".
                     $frm->checkbox("new_permissions[".PHORUM_USER_ALLOW_MODERATE_USERS."]", 1, "Moderate Users")."&nbsp;&nbsp;";
 
-    
+
         $arr[]="Add A Forum...";
-        foreach($forums as $forum_id=>$forum){
-            if(empty($group["permissions"][$forum_id]) && $forum['folder_flag'] == 0)
-                $arr[$forum_id]=$forum["name"];
+        foreach($forumpaths as $forum_id=>$forumname){
+            if(empty($group["permissions"][$forum_id]) && $forums[$forum_id]['folder_flag'] == 0)
+                $arr[$forum_id]=$forumname;
         }
-        
+
         if(count($arr)>1)
             $frm->addrow($frm->select_tag("new_forum", $arr), $perm_frm);
 
@@ -208,7 +208,7 @@
 
                 $row=$frm->addrow($forums[$forum_id]["name"]."<br />".$frm->checkbox("delforum[$forum_id]", 1, "Delete"), $perm_frm);
 
-            }     
+            }
         }
 
         $frm->show();
@@ -218,34 +218,34 @@
     if(empty($_REQUEST["edit"])){
 
         $frm =& new PhorumInputForm ("", "post");
-    
+
         $frm->addbreak("Phorum Group Admin");
-    
+
         $frm->hidden("module", "groups");
-    
+
         $frm->hidden("section", "add");
-    
+
         $frm->addrow("Add A Group:", $frm->text_box("group_name", "", 50));
-    
+
         $frm->show();
 
         echo "<hr class=\"PhorumAdminHR\" />";
         echo "<form action=\"{$_SERVER['PHP_SELF']}\" method=\"post\">\n";
-        echo "<input type=\"hidden\" name=\"module\" value=\"groups\">\n";            
+        echo "<input type=\"hidden\" name=\"module\" value=\"groups\">\n";
         echo "<input type=\"hidden\" name=\"action\" value=\"deleteGroups\">\n";
         echo "<table border=\"0\" cellspacing=\"1\" cellpadding=\"0\" class=\"PhorumAdminTable\" width=\"100%\">\n";
         echo "<tr>\n";
         echo "    <td class=\"PhorumAdminTableHead\">Group</td>\n";
-        echo "    <td class=\"PhorumAdminTableHead\">Delete</td>\n";     
+        echo "    <td class=\"PhorumAdminTableHead\">Delete</td>\n";
         echo "</tr>\n";
-    
+
         foreach($groups as $group){
             echo "<tr>\n";
             echo "    <td class=\"PhorumAdminTableRow\"><a href=\"$_SERVER[PHP_SELF]?module=groups&edit=1&group_id={$group['group_id']}\">".htmlspecialchars($group['name'])."</a></td>\n";
-            echo "    <td class=\"PhorumAdminTableRow\">Delete? <input type=\"checkbox\" name=\"deleteIds[]\" value=\"{$group['group_id']}\"></td>\n";  
+            echo "    <td class=\"PhorumAdminTableRow\">Delete? <input type=\"checkbox\" name=\"deleteIds[]\" value=\"{$group['group_id']}\"></td>\n";
             echo "</tr>\n";
         }
         echo "<tr><td colspan=\"2\" align=\"right\"><input type=\"submit\" name=\"submit\" value=\"Delete Selected\"></td></tr>";
         echo "</table></form>\n";
-    
+
     }
