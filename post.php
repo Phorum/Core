ac5f2fd3e394df64402ea64c61354d6c06c37f94
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2006-10-05 11:47:18 +0000

    MFB: # Added dummy post.php to overwrite existing post.php script (spam prevention)

---
+++ post.php
@@ -0,0 +1,8 @@
+<?php
+# The script "post.php" is no longer used by Phorum. It has been replaced 
+# by the new posting.php script. This file is included in the distribution
+# to make sure that in a Phorum 5.0 upgrade the post.php script is overwritten,
+# to prevent spammers from using the old post.php script directly.
+
+die ("post.php has been replaced by posting.php");
+?>
