17105ea6a350cc2ba26dd2deb6988a27e987fd90
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-02-15 15:39:26 +0000

    # Added some basic settings for controlling the displaying of announcements. # Show Only Unread? : If checked, then only announcements with unread messages are shown # Maximum Days To Show : Announcement threads older than this number of days are not shown

--- mods/announcements/announcements.php
+++ mods/announcements/announcements.php
@@ -49,6 +49,11 @@ function phorum_show_announcements ()
         // Process the announcements.
         foreach($messages as $message)
         {
+            // Skip this message if it's older than the number of days that was
+            // configured in the settings screen.
+            if (!empty($PHORUM["mod_announcements"]["days_to_show"]) &&
+                $message["datestamp"] < (time()-($PHORUM["mod_announcements"]["days_to_show"]*86400))) continue;
+
             // Check if there are new messages in the thread.
             if (isset($newinfo)) {
                 $new = 0;
@@ -59,10 +64,16 @@ function phorum_show_announcements ()
                         break;
                     }
                 }
+
                 // There are new messages. Setup the template data for showing a new flag.
                 if ($new) {
                     $message["new"] = ($new ? $PHORUM["DATA"]["LANG"]["newflag"] : NULL);
                     $message["URL"]["NEWPOST"] = phorum_get_url(PHORUM_FOREIGN_READ_URL, $message["forum_id"], $message["thread"],"gotonewpost");
+                }
+                // No new messages. Skip this thread if only unread announcement messages
+                // have to be shown.
+                elseif ($PHORUM["mod_announcements"]["only_show_unread"]) {
+                    continue;
                 }
             }
 
@@ -73,6 +84,9 @@ function phorum_show_announcements ()
             $message["URL"]["READ"] = phorum_get_url(PHORUM_FOREIGN_READ_URL, $message["forum_id"], $message["message_id"]);
             $PHORUM["DATA"]["ANNOUNCEMENTS"][] = $message;
         }
+
+        // If all announcements were skipped, then we are done.
+        if (!isset($PHORUM["DATA"]["ANNOUNCEMENTS"])) return;
 
         // Apply standard formatting to the messages.
         $PHORUM["DATA"]["ANNOUNCEMENTS"] = phorum_hook("format", $PHORUM["DATA"]["ANNOUNCEMENTS"]);
--- mods/announcements/settings.php
+++ mods/announcements/settings.php
@@ -7,14 +7,16 @@ $error="";
 if(count($_POST)){
 
     $_POST["number_to_show"] = (int)$_POST["number_to_show"];
-    if($_POST["number_to_show"]==0){
+    if((int)$_POST["number_to_show"]==0){
         // sanity check
         $_POST["number_to_show"] = 5;
     }
 
     $PHORUM["mod_announcements"]["forum_id"] = (int)$_POST["forum_id"];
     $PHORUM["mod_announcements"]["pages"] = $_POST["pages"];
-    $PHORUM["mod_announcements"]["number_to_show"] = $_POST["number_to_show"];
+    $PHORUM["mod_announcements"]["only_show_unread"] = $_POST["only_show_unread"];
+    $PHORUM["mod_announcements"]["number_to_show"] = (int)$_POST["number_to_show"];
+    $PHORUM["mod_announcements"]["days_to_show"] = (int)$_POST["days_to_show"];
 
     if(empty($error)){
         if(!phorum_db_update_settings(array("mod_announcements"=>$PHORUM["mod_announcements"]))){
@@ -39,13 +41,15 @@ $forum_list = phorum_get_forum_info(true);
 $frm->addrow("Announcement Forum", $frm->select_tag("forum_id", $forum_list, $PHORUM["mod_announcements"]["forum_id"]));
 
 
-$page_list = $frm->checkbox("pages[index]", 1, "Forum List (index.php)", $PHORUM["mod_announcements"]["pages"]["index"])."&nbsp;&nbsp;".
-             $frm->checkbox("pages[list]", 1, "Message List (list.php)", $PHORUM["mod_announcements"]["pages"]["list"])."&nbsp;&nbsp;".
-             $frm->checkbox("pages[read]", 1, "Read Message (read.php)", $PHORUM["mod_announcements"]["pages"]["read"])."&nbsp;&nbsp;";
+$page_list = $frm->checkbox("pages[index]", 1, "Forum List (index.php)", $PHORUM["mod_announcements"]["pages"]["index"])."<br/>".
+             $frm->checkbox("pages[list]", 1, "Message List (list.php)", $PHORUM["mod_announcements"]["pages"]["list"])."<br/>".
+             $frm->checkbox("pages[read]", 1, "Read Message (read.php)", $PHORUM["mod_announcements"]["pages"]["read"]);
 
-$frm->addrow("Pages Announcements Appear", $page_list);
+$frm->addrow("Announcements Appear On", $page_list);
 
+$frm->addrow("Show Only Unread?", $frm->checkbox("only_show_unread", 1, "", $PHORUM["mod_announcements"]["only_show_unread"]));
 $frm->addrow("Number To Show", $frm->text_box("number_to_show", $PHORUM["mod_announcements"]["number_to_show"], 10));
+$frm->addrow("Maximum Days To Show", $frm->text_box("days_to_show", $PHORUM["mod_announcements"]["days_to_show"], 10) . " (0 = forever)");
 
 $frm->show();
 
