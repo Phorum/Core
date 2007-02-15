11fba60e49fb0414c5617b4603fcab82c671418e
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-02-15 13:05:49 +0000

    # Moved stylesheet code to the module templates instead of using a global styles.css

--- mods/announcements/announcements.php
+++ mods/announcements/announcements.php
@@ -6,10 +6,15 @@ function phorum_setup_announcements ()
 {
     global $PHORUM;
 
-    if(isset($PHORUM["mod_announcements"]["forum_id"]) && $PHORUM["forum_id"]!=$PHORUM["mod_announcements"]["forum_id"]){
-        if(!empty($PHORUM["mod_announcements"]["pages"][phorum_page])){
-            $PHORUM["DATA"]["HEAD_TAGS"] .= "\n<link rel=\"stylesheet\" type=\"text/css\" href=\"".$PHORUM["http_path"]."/mods/announcements/styles.css\" media=\"screen\" />";
-        }
+    if(isset($PHORUM["mod_announcements"]["forum_id"]) && $PHORUM["forum_id"]!=$PHORUM["mod_announcements"]["forum_id"] && !empty($PHORUM["mod_announcements"]["pages"][phorum_page])){
+
+        // Inlcude style sheet information for this module, if the css template isn't empty.
+        ob_start();
+        include phorum_get_template("announcements::css");
+        $css = ob_get_contents();
+        ob_end_clean();
+        if ($css != '') 
+          $PHORUM["DATA"]["HEAD_TAGS"] .= "<style type=\"text/css\" media=\"screen\">$css</style>";
     }
 }
 
@@ -17,9 +22,7 @@ function phorum_show_announcements ()
 {
     $PHORUM=$GLOBALS["PHORUM"];
 
-    if(isset($PHORUM["mod_announcements"]["forum_id"]) && $PHORUM["forum_id"]!=$PHORUM["mod_announcements"]["forum_id"]){
-
-        if(!empty($PHORUM["mod_announcements"]["pages"][phorum_page])){
+    if(isset($PHORUM["mod_announcements"]["forum_id"]) && $PHORUM["forum_id"]!=$PHORUM["mod_announcements"]["forum_id"] && !empty($PHORUM["mod_announcements"]["pages"][phorum_page])){
 
             $messages = phorum_db_get_recent_messages($PHORUM["mod_announcements"]["number_to_show"], $PHORUM["mod_announcements"]["forum_id"], 0, true);
 
@@ -41,7 +44,6 @@ function phorum_show_announcements ()
 
             include phorum_get_template("announcements::announcements");
 
-        }
     }
 }
 
