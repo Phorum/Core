253986c5cc1ecea01069561cd26d14e75a458218
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-04-11 00:45:01 +0000

    # Updated copyright notices to 2007

--- include/admin/file_purge.php
+++ include/admin/file_purge.php
@@ -2,7 +2,7 @@
     
 ////////////////////////////////////////////////////////////////////////////////
 //                                                                            //
-//   Copyright (C) 2006  Phorum Development Team                              //
+//   Copyright (C) 2007  Phorum Development Team                              //
 //   http://www.phorum.org                                                    //
 //                                                                            //
 //   This program is free software. You can redistribute it and/or modify     //
