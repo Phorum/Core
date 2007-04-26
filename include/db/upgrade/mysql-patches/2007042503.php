b35b8de4cfa6e73fb2001f188da6b9746a0ed2a8
ts77 <ts77@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-04-26 06:26:11 +0000

    # add forum-paths on upgrade.

---
+++ include/db/upgrade/mysql-patches/2007042503.php
@@ -0,0 +1,15 @@
+<?php
+if(!defined("PHORUM_ADMIN")) return;
+
+// rebuild the forum-paths for each and every forum in the tree
+$forums = phorum_admin_build_path_array();
+unset($forums[0]);
+
+foreach($forums as $fid => $forumpath) {
+    
+    $update_forum=array('forum_id'=>$fid, 'forum_path'=>$forumpath);
+    phorum_db_update_forum($update_forum);
+    
+}
+
+?>
