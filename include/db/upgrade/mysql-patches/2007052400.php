b466fef83952ccaae5a291598de636fa9d77caba
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-05-24 19:23:52 +0000

    Oops... INTs should have had a zero default,

--- include/db/upgrade/mysql-patches/2007052400.php
+++ include/db/upgrade/mysql-patches/2007052400.php
@@ -3,9 +3,9 @@
 $upgrade_queries[]= 
     "CREATE TABLE {$PHORUM['message_tracking_table']} (
 			track_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-			message_id INT UNSIGNED NOT NULL DEFAULT '',
-			user_id INT UNSIGNED NOT NULL DEFAULT '',
-			time INT UNSIGNED NOT NULL DEFAULT '',
+			message_id INT UNSIGNED NOT NULL DEFAULT '0',
+			user_id INT UNSIGNED NOT NULL DEFAULT '0',
+			time INT UNSIGNED NOT NULL DEFAULT '0',
 			diff_body TEXT NULL ,
 			diff_subject TEXT NULL ,
 			
