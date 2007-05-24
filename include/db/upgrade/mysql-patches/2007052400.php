262ebabb9d821df09ac783c180553c4d5443458a
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-05-24 18:52:58 +0000

    # Added defaults for NOT NULL fields (for strict mysql)

--- include/db/upgrade/mysql-patches/2007052400.php
+++ include/db/upgrade/mysql-patches/2007052400.php
@@ -3,9 +3,9 @@
 $upgrade_queries[]= 
     "CREATE TABLE {$PHORUM['message_tracking_table']} (
 			track_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-			message_id INT UNSIGNED NOT NULL ,
-			user_id INT UNSIGNED NOT NULL ,
-			time INT UNSIGNED NOT NULL ,
+			message_id INT UNSIGNED NOT NULL DEFAULT '',
+			user_id INT UNSIGNED NOT NULL DEFAULT '',
+			time INT UNSIGNED NOT NULL DEFAULT '',
 			diff_body TEXT NULL ,
 			diff_subject TEXT NULL ,
 			
