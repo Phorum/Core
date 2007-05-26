f5d65ca7d49511a3114d47508f49526dd70240f8
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-05-26 20:53:19 +0000

    # Fixed a double @return tag in phpdoc for phorum_get_template_file().

--- common.php
+++ common.php
@@ -584,13 +584,19 @@ function phorum_check_read_common()
 /**
  * Find out what input and output files to use for a template file.
  *
- * @param $page - The template base name (e.g. "header", "css", etc.).
- * @param $module - The module to load the template from or NULL if the
- *                main Phorum templates directory must be used.
- * @return $phpfile - The PHP file to include for the template base name.
- * @return $tplfile - The file to use as template input. In case there's no
- *                .tpl file to pre-process, the value will be NULL. In that
- *                case, the $phpfile return value can be included directly.
+ * @param string $page
+ *     The template base name (e.g. "header", "css", etc.).
+ *
+ * @param string $module
+ *     The module to load the template from or NULL if the
+ *     main Phorum templates directory must be used.
+ *
+ * @return array
+ *     This function returns an array, containing two elements:   
+ *     - The PHP file to include for the template base name.
+ *     - The file to use as template input. In case there's no
+ *       .tpl file to pre-process, the value will be NULL. In that
+ *       case, the $phpfile return value can be included directly.
  */
 function phorum_get_template_file( $page )
 {
