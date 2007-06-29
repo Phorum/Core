56d8c0957af73675ba2466817cbb472185c7fc43
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-06-29 14:21:52 +0000

    # Prevent browser mangling of special characters in serialized posting form data

--- posting.php
+++ posting.php
@@ -555,8 +555,11 @@ if ($PHORUM["posting_template"] == 'posting')
             $val = $mode;
             if ($spec[pf_SIGNED]) $signval = $mode;
         } elseif ($spec[pf_TYPE] == "array") {
-            $val = htmlspecialchars(serialize($message[$var]));
-            if ($spec[pf_SIGNED]) $signval = serialize($message[$var]);
+            // base64_encode to convert newlines into data that can be
+            // tranferred safely back and forth to the browser, without
+            // getting converted (e.g. \r\n to \n).
+            $val = base64_encode(serialize($message[$var]));
+            if ($spec[pf_SIGNED]) $signval = $val;
         } else {
             $val = htmlspecialchars($message[$var]);
             if ($spec[pf_SIGNED]) $signval = $message[$var];
