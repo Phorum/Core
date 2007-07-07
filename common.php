2b7543274b54da0ab417a7dc240a246bf9eefd02
brian <brian@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-07-07 17:57:55 +0000

    # fixed output cleaning for zlib systems

--- common.php
+++ common.php
@@ -1072,7 +1072,7 @@ function phorum_database_error($error)
 
     // Flush output that we buffered so far (for displaying a
     // clean page in the admin interface).
-    while (ob_get_level()) ob_end_clean();
+    while (@ob_end_clean());
 
     // Give modules a chance to handle or process the database error.
     phorum_hook("database_error", $error);
