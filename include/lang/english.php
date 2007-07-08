c8ebb2cd742a24a104eadf850e3bee765f2cea08
ts77 <ts77@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-07-08 09:44:42 +0000

    # added back long_date_time which was remove (probably accidently) in [2115]

--- include/lang/english.php
+++ include/lang/english.php
@@ -6,7 +6,7 @@
 
     // check the php-docs for the syntax of these entries (http://www.php.net/manual/en/function.strftime.php)
     // One tip, don't use T for showing the time zone as users can change their time zone.
-
+    $PHORUM['long_date_time']="%B %d, %Y %I:%M%p";
     $PHORUM['short_date_time']="%m/%d/%Y %I:%M%p";
     $PHORUM['long_date']="%B %d, %Y";
     $PHORUM['short_date']="%m/%d/%Y";
