4245623fd0a91ed11b6df8febe7d48df7dc1d088
ts77 <ts77@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-07-13 20:37:08 +0000

    # eventlogging fixes for mysql4

--- mods/event_logging/db.php
+++ mods/event_logging/db.php
@@ -10,7 +10,7 @@ require_once("./mods/event_logging/constants.php");
 define("EVENT_LOGGING_DB_VERSION", 1);
 
 // The table name for storing event logs.
-$GLOBALS["PHORUM"]["event_logging_table"] = 
+$GLOBALS["PHORUM"]["event_logging_table"] =
     "{$GLOBALS["PHORUM"]["DBCONFIG"]["table_prefix"]}_event_logging";
 
 /**
@@ -21,7 +21,7 @@ function event_logging_db_install()
 {
     $PHORUM = $GLOBALS["PHORUM"];
 
-    $version = isset($PHORUM["mod_event_logging_installed"]) 
+    $version = isset($PHORUM["mod_event_logging_installed"])
         ? $PHORUM["mod_event_logging_installed"] : 0;
 
     while ($version < EVENT_LOGGING_DB_VERSION)
@@ -32,23 +32,23 @@ function event_logging_db_install()
 
         $sqlfile = "./mods/event_logging/db/" .
                    $PHORUM["DBCONFIG"]["type"] . "/$version.php";
-                   
+
         if (! file_exists($sqlfile)) {
             print "<b>Unexpected situation on installing " .
                   "the Event Logging module</b>: " .
-                  "unable to find the database schema setup script " . 
+                  "unable to find the database schema setup script " .
                   htmlspecialchars($sqlfile);
             return false;
         }
 
         $sqlqueries = array();
         include($sqlfile);
-        
+
         if (count($sqlqueries) == 0) {
             print "<b>Unexpected situation on installing " .
                   "the Event Logging module</b>: could not read any SQL " .
                   "queries from file " . htmlspecialchars($sqlfile);
-            return false;                    
+            return false;
         }
         $err = phorum_db_run_queries($sqlqueries);
         if ($err) {
@@ -56,7 +56,7 @@ function event_logging_db_install()
                   "the Event Logging module</b>: running the " .
                   "install queries from file " . htmlspecialchars($sqlfile) .
                   " failed. The error was " . htmlspecialchars($err);
-            return false;                    
+            return false;
         }
 
         // Save our settings.
@@ -86,13 +86,13 @@ function event_logging_db_install()
  *                   details     Details about the log message, which can
  *                               span multiple lines. This could for example
  *                               be used for providing a debug backtrace.
- *                   source      The source of the log message. This is a 
+ *                   source      The source of the log message. This is a
  *                               free 32 char text field, which can be used
  *                               to specifiy what part of Phorum generated the
  *                               log message (e.g. "mod_smileys"). If no
  *                               source is provided, the "phorum_page"
  *                               constant will be used instead.
- *                   category    A high level category for the message. 
+ *                   category    A high level category for the message.
  *                               Options for this field are:
  *                               EVENTLOG_CAT_APPLICATION (default)
  *                               EVENTLOG_CAT_DATABASE
@@ -100,8 +100,8 @@ function event_logging_db_install()
  *                               EVENTLOG_CAT_SYSTEM
  *                               EVENTLOG_CAT_MODULE
  *                   loglevel    This indicates the severety of the message.
- *                               Options for this field are: 
- *                               EVENTLOG_LVL_DEBUG   
+ *                               Options for this field are:
+ *                               EVENTLOG_LVL_DEBUG
  *                                 Messages that are used by programmers
  *                                 for tracking low level Phorum operation.
  *                               EVENTLOG_LVL_INFO
@@ -124,7 +124,7 @@ function event_logging_db_install()
  *                                 Alert messages indicate errors which should
  *                                 be corrected as soon as possible (e.g. loss
  *                                 of network connectivity or a full disk).
- *                                 These should be relayed to the system 
+ *                                 These should be relayed to the system
  *                                 administrator).
  *
  *                   vroot       vroot for which a message is generated.
@@ -179,7 +179,7 @@ function event_logging_writelog($loginfo)
             case "hostname":
                 $record[$key] = "'" . phorum_db_interact(DB_RETURN_QUOTED, $val) . "'";
                 break;
-        
+
             default: phorum_database_error(
                 "event_logging_log(): Illegal key " .
                 "field \"$key\" in the \$loginfo argument"
@@ -190,13 +190,13 @@ function event_logging_writelog($loginfo)
     // Add the message source.
     $from_module = FALSE;
     if (!isset($record["source"])) {
-        list($source, $from_module) = event_logging_find_source(1);   
+        list($source, $from_module) = event_logging_find_source(1);
         $record["source"] = "'" . phorum_db_interact(DB_RETURN_QUOTED, $source) . "'";
     }
 
     // Add the category.
     if (!isset($record["category"])) {
-        $record["category"] = $from_module 
+        $record["category"] = $from_module
                             ? EVENTLOG_CAT_MODULE
                             : EVENTLOG_CAT_APPLICATION;
     }
@@ -235,7 +235,7 @@ function event_logging_writelog($loginfo)
     // Insert the logging record in the database.
     phorum_db_interact(
         DB_RETURN_RES,
-        "INSERT INTO {$PHORUM["event_logging_table"]} 
+        "INSERT INTO {$PHORUM["event_logging_table"]}
                 (".implode(', ', array_keys($record)).")
          VALUES (".implode(', ', $record).")"
     );
@@ -246,7 +246,7 @@ function event_logging_writelog($loginfo)
  * description.
  *
  * @param $filter - The filter description.
- * 
+ *
  * @return $where - A WHERE statement, representing the filter description.
  */
 function event_logging_create_where($filter)
@@ -254,7 +254,7 @@ function event_logging_create_where($filter)
     if ($filter === NULL || !is_array($filter)) return '';
 
     $where_parts = array();
- 
+
     if (isset($filter["loglevels"])) {
         $where = '';
         if (is_array($filter["loglevels"]) && count($filter["loglevels"]))
@@ -290,7 +290,7 @@ function event_logging_create_where($filter)
     foreach (array(
         "source"   => "string",
         "user_id"  => "int",
-        "username" => "string", 
+        "username" => "string",
         "ip"       => "string",
     ) as $field => $type) {
         if (isset($filter[$field]) && trim($filter[$field]) != '') {
@@ -312,7 +312,7 @@ function event_logging_create_where($filter)
 
     $where = '';
     if (count($where_parts) > 0) {
-        $where = 'WHERE ' . implode(' AND ', $where_parts); 
+        $where = 'WHERE ' . implode(' AND ', $where_parts);
     }
 
     return $where;
@@ -362,11 +362,11 @@ function event_logging_countlogs($filter = NULL)
         "SELECT count(*) AS count
          FROM   {$PHORUM["event_logging_table"]}
                 LEFT JOIN {$PHORUM["user_table"]}
-                USING (user_id)
+                ON {$PHORUM["user_table"]}.user_id = {$PHORUM["event_logging_table"]}.user_id
          $where"
     );
 
-    $count = $rows[0]['count']; 
+    $count = $rows[0]['count'];
 
     // Limit the maximum number of event logs.
     $max = (int)$PHORUM["mod_event_logging"]["max_log_entries"];
@@ -397,9 +397,9 @@ function event_logging_countlogs($filter = NULL)
     return $count;
 }
 
-/** 
+/**
  * Retrieve event logs from the database, either for a filtered or an
- * unfiltered view. 
+ * unfiltered view.
  *
  * @param $page       - The page number to retrieve logs for.
  * @param $pagelength - The number of log messages per page.
@@ -438,9 +438,9 @@ function event_logging_getlogs($page = 1, $pagelength = 20, $filter = NULL)
                    {$PHORUM["forums_table"]}.name              AS forum
             FROM   {$PHORUM["event_logging_table"]}
                    LEFT JOIN {$PHORUM["user_table"]}
-                   USING (user_id)
+                   ON {$PHORUM["user_table"]}.user_id) = {$PHORUM["event_logging_table"]}.user_id
                    LEFT JOIN {$PHORUM["forums_table"]}
-                   USING (forum_id)
+                   ON {$PHORUM["forums_table"]}.forum_id = {$PHORUM["event_logging_table"]}.forum_id
             $where
             ORDER  BY log_id DESC
             LIMIT  $pagelength
@@ -460,7 +460,7 @@ function event_logging_clearlogs($filter = NULL)
     $PHORUM = $GLOBALS["PHORUM"];
 
     $where = event_logging_create_where($filter);
- 
+
     phorum_db_interact(
         DB_RETURN_RES,
         "DELETE FROM {$PHORUM["event_logging_table"]} $where"
