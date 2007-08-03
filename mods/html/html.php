7264e805b5c18798cfa9a46c31c8de0c62e40b11
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-08-03 23:17:28 +0000

    # Fixed <phorum break> stripping. # For 5.2, &quot; needed restoring too, because of switching to # htmlspecialchars() with charset support for escaping html code.

--- mods/html/html.php
+++ mods/html/html.php
@@ -5,13 +5,18 @@ if(!defined("PHORUM")) return;
 // HTML Phorum Mod
 function phorum_html($data)
 {
-	$PHORUM = $GLOBALS["PHORUM"];
+    $PHORUM = $GLOBALS["PHORUM"];
 
-	foreach($data as $message_id => $message){
-
-        if(isset($message["body"])){
-
+    foreach($data as $message_id => $message)
+    {
+        if(isset($message["body"]))
+        {
             $body = $message["body"];
+
+            // Protect against poisoned null byte XSS attacks
+            // (MSIE does not protect itself against these, so we have
+            // to take care of that).
+            str_replace("\0", "", $body);
 
             // restore tags where Phorum has killed them
             $body = preg_replace("!&lt;(\/*[a-z].*?)&gt;!i", "<$1>", $body);
@@ -30,22 +35,24 @@ function phorum_html($data)
 
             // turn script and meta tags into comments
             $body=preg_replace("/<(\/*(script|meta).*?)>/i", "<!--$1-->", $body);
-
-            // strip any <br phorum=\"true\" /> that got inside certain blocks like tables and pre.
+            // strip any <phorum break> tags that got inside certain
+            // blocks like tables (to prevent <table><br/><tr> like
+            // code) and pre/xmp (newlines are shown, even without
+            // <br/> tags).
             $block_tags="table|pre|xmp";
 
             preg_match_all("!(<($block_tags).*?>).+?(</($block_tags).*?>)!ms", $body, $matches);
 
             foreach($matches[0] as $block){
-                $newblock=str_replace("<br phorum=\"true\" />", "", $block);
+                $newblock=str_replace("<phorum break>", "", $block);
                 $body=str_replace($block, $newblock, $body);
             }
 
-    		$data[$message_id]["body"] = $body;
+            $data[$message_id]["body"] = $body;
         }
-	}
+    }
 
-	return $data;
+    return $data;
 }
 
 ?>
