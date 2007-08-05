0bfaf1a0b5b72675ef0f779722c1d2376dce85c1
brian <brian@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-08-05 04:47:38 +0000

    # more fixes for possible XSS issues in HTML module when used with other formatting modules

--- mods/html/html.php
+++ mods/html/html.php
@@ -13,29 +13,38 @@ function phorum_html($data)
         {
             $body = $message["body"];
 
+            // pull out the phorum breaks
+            $body = str_replace("<phorum break>", "", $body);
+
             // Protect against poisoned null byte XSS attacks
             // (MSIE does not protect itself against these, so we have
             // to take care of that).
             str_replace("\0", "", $body);
 
             // restore tags where Phorum has killed them
-            $body = preg_replace("!&lt;(\/*[a-z].*?)&gt;!i", "<$1>", $body);
+            $body = preg_replace("!&lt;(\/*[a-z].*?)&gt;!si", "<$1>", $body);
 
             // restore escaped & and "
             $body = str_replace("&amp;", "&", $body);
             $body = str_replace("&quot;", '"', $body);
 
             // strip out javascript events
-            if(preg_match_all("/<[a-z][^>]+>/i", $body, $matches)) {
+            if(preg_match_all("/<[a-z][^>]+>/si", $body, $matches)){
                 $tags=array_unique($matches[0]);
-                foreach($tags as $tag) {
-                    $newtag=preg_replace("/\son.+?=[^>]+/i", "$1", $tag);
+                foreach($tags as $tag){
+                    $newtag=str_replace("\n", " ", $tag);
+                    $newtag=preg_replace("/\son[^=]+=\".+?\"/is", "$1", $newtag);
+                    $newtag=preg_replace("/\son[^=]+='.+?'/is", "$1", $newtag);
+                    $newtag=preg_replace("/\son[^=]+=[^'\"][^\s>]+/si", "$1", $newtag);
                     $body=str_replace($tag, $newtag, $body);
                 }
             }
 
             // turn script and meta tags into comments
-            $body=preg_replace("/<(\/*(script|meta).*?)>/i", "<!--$1-->", $body);
+            $body=preg_replace("/<(\/*(script|meta).*?)>/si", "<!--$1-->", $body);
+
+            // put the phorum breaks back
+            $body = str_replace("\n", "<phorum break>\n", $body);
 
             // strip any <phorum break> tags that got inside certain
             // blocks like tables (to prevent <table><br/><tr> like
