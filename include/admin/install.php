2f973b1e11a05b00e6af713fdd00669674e2326f
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-08-07 11:56:01 +0000

    # Typo fix (thanks Azumandias)

--- include/admin/install.php
+++ include/admin/install.php
@@ -329,7 +329,7 @@ switch ($step){
             // set initial settings
             $settings=array(
             "title" => "Phorum 5",
-            "description" => "Congratulations!  You have installed Phorum 5!  To change this text, go to your admin, choose General Settings and changed the description",
+            "description" => "Congratulations!  You have installed Phorum 5!  To change this text, go to your admin, choose General Settings and change the description",
             "cache" => $default_cache_dir,
             "session_timeout" => "30",
             "short_session_timeout" => "60",
