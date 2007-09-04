665d2671ac7fbbc115d6aa3210b9700b39ccf244
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-09-04 21:46:15 +0000

    # Temporary change to debug an upgrade problem.

--- include/db/upgrade/mysql-patches/2007050901.php
+++ include/db/upgrade/mysql-patches/2007050901.php
@@ -1,6 +1,7 @@
 <?php 
 
 // Find all private messages.
+print "Start select of private messages\n";
 $res = phorum_db_interact(
     DB_RETURN_RES,
     "SELECT pm_message_id, user_id, meta
@@ -8,12 +9,15 @@ $res = phorum_db_interact(
 );
 
 // Update the meta author + rcpt info for each private message.
+print "Start loop\n";
 while ($row = phorum_db_fetch_row($res, DB_RETURN_ASSOC))
 {
     // Update the PM author.
     $m = $row["pm_message_id"]; 
     $u = $row["user_id"];
+    print "Handle message $m for author id $u\n";
     $user = phorum_db_user_get($u);
+    print "> update message\n";
     if ($user) {
         $author = phorum_db_interact(DB_RETURN_QUOTED, $user["display_name"]);
         phorum_db_interact(
@@ -25,6 +29,7 @@ while ($row = phorum_db_fetch_row($res, DB_RETURN_ASSOC))
     }
 
     // Update the PM recipients.
+    print "> update PM rcpts\n";
     $meta = unserialize($row["meta"]);
     $userids = array_keys($meta["recipients"]);
     $users = phorum_db_user_get($userids);
@@ -34,6 +39,7 @@ while ($row = phorum_db_fetch_row($res, DB_RETURN_ASSOC))
             $user["display_name"];
     }
     $meta = phorum_db_interact(DB_RETURN_QUOTED, serialize($meta));
+    print "> store new data\n";
     phorum_db_interact(
         DB_RETURN_RES,
         "UPDATE {$PHORUM["pm_messages_table"]}
