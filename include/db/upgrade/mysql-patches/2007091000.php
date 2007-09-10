fb9eb539a5abcabeb94ffe94f406bce4fcdc49a0
brian <brian@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-09-10 23:57:00 +0000

    # fixed bad table name

--- include/db/upgrade/mysql-patches/2007091000.php
+++ include/db/upgrade/mysql-patches/2007091000.php
@@ -1,8 +1,8 @@
 <?php
 
-$upgrade_queries[]= "alter table {$PHORUM['messages_table']} add `moved` tinyint(1) NOT NULL default '0'";
-$upgrade_queries[]= "update {$PHORUM['messages_table']} set moved=1 where parent_id=0 and thread!=message_id";
-$upgrade_queries[]= "alter table {$PHORUM['messages_table']} add key new_count (forum_id, status, moved, message_id)";
-$upgrade_queries[]= "alter table {$PHORUM['messages_table']} drop key post_count, add key new_threads (forum_id, status, parent_id, moved, message_id)";
+$upgrade_queries[]= "alter table {$PHORUM['message_table']} add `moved` tinyint(1) NOT NULL default '0'";
+$upgrade_queries[]= "update {$PHORUM['message_table']} set moved=1 where parent_id=0 and thread!=message_id";
+$upgrade_queries[]= "alter table {$PHORUM['message_table']} add key new_count (forum_id, status, moved, message_id)";
+$upgrade_queries[]= "alter table {$PHORUM['message_table']} drop key post_count, add key new_threads (forum_id, status, parent_id, moved, message_id)";
 
 ?>
