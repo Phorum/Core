396d7a31d175e2fa89a69f6c9d5ddb1deca4154f
brian <brian@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-09-10 23:53:11 +0000

    # missed these keys

--- include/db/upgrade/mysql-patches/2007091000.php
+++ include/db/upgrade/mysql-patches/2007091000.php
@@ -2,6 +2,7 @@
 
 $upgrade_queries[]= "alter table {$PHORUM['messages_table']} add `moved` tinyint(1) NOT NULL default '0'";
 $upgrade_queries[]= "update {$PHORUM['messages_table']} set moved=1 where parent_id=0 and thread!=message_id";
-
+$upgrade_queries[]= "alter table {$PHORUM['messages_table']} add key new_count (forum_id, status, moved, message_id)";
+$upgrade_queries[]= "alter table {$PHORUM['messages_table']} drop key post_count, add key new_threads (forum_id, status, parent_id, moved, message_id)";
 
 ?>
