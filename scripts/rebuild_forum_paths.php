76d0fa80c0621fe8bf62fb5a4ce8bf599b8dffb5
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-09-20 12:05:06 +0000

    # Added script to rebuild forum_path data. Can be needed after doing a # conversion, where only phorum_db_add_forum() was used (path info is built # on forum update calls only).

---
+++ scripts/rebuild_forum_paths.php
@@ -0,0 +1,30 @@
+<?php
+// rebuild path info
+// this script rebuilds the path info data for all forums 
+
+// if we are running in the webserver, bail out
+if (isset($_SERVER["REMOTE_ADDR"])) {
+    echo "This script cannot be run from a browser.";
+    return;
+}
+
+define('phorum_page', 'rebuild_forum_paths');
+
+chdir(dirname(__FILE__) . "/..");
+require_once './common.php';
+include_once( "./include/admin_functions.php" );
+
+print "\nRebuilding forum path info ...\n";
+
+$forums = phorum_admin_build_path_array();
+unset($forums[0]);
+
+foreach($forums as $fid => $forumpath)
+{
+    $update_forum = array('forum_id'=>$fid, 'forum_path'=>$forumpath);
+    phorum_db_update_forum($update_forum);
+}
+
+print "\n";
+
+?>
