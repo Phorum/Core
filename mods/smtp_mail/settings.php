3ca474174687702e8c63d11a3adf36b42b1bffa0
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-09-26 22:58:28 +0000

    # Some small textual and code updates for the SMTP mail module.

--- mods/smtp_mail/settings.php
+++ mods/smtp_mail/settings.php
@@ -20,24 +20,23 @@ if(count($_POST)){
                                  'conn' => $_POST['conn']
                                  );
 
-    if(empty($error)){
-        if(!phorum_db_update_settings(array("smtp_mail"=>$PHORUM["smtp_mail"]))){
-            $error="Database error while updating settings.";
-        } else {
-            echo "Settings Updated<br />";
-        }
+    if (empty($error)) {
+        phorum_db_update_settings(array(
+            "smtp_mail" => $PHORUM["smtp_mail"]
+        ));
+        phorum_admin_okmsg("Settings updated");
     }
 }
 
 include_once "./include/admin/PhorumInputForm.php";
 
-$frm =& new PhorumInputForm ("", "post", "Save");
+$frm = new PhorumInputForm ("", "post", "Save");
 
 $frm->hidden("module", "modsettings");
 
 $frm->hidden("mod", "smtp_mail");
 
-$frm->addbreak("Settings for the SMTP-Mail-Module");
+$frm->addbreak("Settings for the SMTP Mail Module");
 
 
 $frm->addrow("Hostname of mailserver", $frm->text_box("host", $PHORUM['smtp_mail']['host'], 50));
@@ -46,10 +45,10 @@ $frm->addrow("Port of mailserver", $frm->text_box("port", $PHORUM['smtp_mail']['
 $frm->addrow("Connection Type", $frm->select_tag("conn", array('auto'=>'Auto Detect','plain'=>'Plain Connection','ssl'=>'SSL-Encryption','tls'=>'TLS-Encryption'), $PHORUM['smtp_mail']['conn'])." (e.g. Google-Mail connection needs TLS)");
 
 
-$frm->addrow("Use SMTP-Auth", $frm->select_tag("auth",array(1=>'Yes',0=>'No'),$PHORUM['smtp_mail']['auth']));
+$frm->addrow("Use SMTP Auth", $frm->select_tag("auth",array(1=>'Yes',0=>'No'),$PHORUM['smtp_mail']['auth']));
 
-$frm->addrow("SMTP-Auth Username", $frm->text_box("auth_username", $PHORUM['smtp_mail']['username'], 50));
-$frm->addrow("SMTP-Auth Password", $frm->text_box("auth_password", $PHORUM['smtp_mail']['password'], 50,0,true));
+$frm->addrow("SMTP Auth Username", $frm->text_box("auth_username", $PHORUM['smtp_mail']['username'], 50));
+$frm->addrow("SMTP Auth Password", $frm->text_box("auth_password", $PHORUM['smtp_mail']['password'], 50,0,true));
 
 $frm->show();
 
