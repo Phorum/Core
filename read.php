5a3f54350edd9166221a48f1f0e84c68678c7370
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-10-08 20:10:04 +0000

    # View counts can update in admin-only status and normal status.

--- read.php
+++ read.php
@@ -684,7 +684,7 @@ if(!empty($data) && isset($data[$thread]) && isset($data[$message_id])) {
 
     // increment viewcount if enabled
     if($PHORUM['count_views'] &&
-      (!isset($PHORUM['status']) || $PHORUM["status"]==PHORUM_MASTER_STATUS_NORMAL)) {
+      (!isset($PHORUM['status']) || $PHORUM["status"]!=PHORUM_MASTER_STATUS_READ_ONLY)) {
         // increment viewcount per thread if enabled
         $inc_thread_id = NULL;
         if (!empty($PHORUM['count_views_per_thread'])) {
