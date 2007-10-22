b64e2dde1eeb12ce3cce26faebaafe77ab1fd0ca
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-10-22 17:02:33 +0000

    Added Turkish language file.

---
+++ mods/bbcode/lang/turkish.php
@@ -0,0 +1,47 @@
+<?php
+$PHORUM["DATA"]["LANG"]["mod_bbcode"] = array(
+    # Button descriptions.
+    'bold'          => 'Kalýn',
+    'italic'        => 'Ýtalik',
+    'underline'     => 'Altý Çizgili',
+    'strike'        => 'Üstü Çizgili',
+    'subscript'     => 'Subscript',
+    'superscript'   => 'Superscript',
+    'color'         => 'Select font color',
+    'size'          => 'Select font size',
+    'center'        => 'Yazýyý Ortala',
+    'url'           => 'Site Adresi Ekle',
+    'email'         => 'E-Posta Adresi Ekle',
+    'image'         => 'Resim ekle',
+    'hr'            => 'Dikey Çizgi Ekle',
+    'code'          => 'Biçimlendirilmiþ Yazý',
+    'quote'         => 'Alýntý',
+
+    # Dialogs and errors.
+    'enter url'     => 'Lütfen baðlantýsýný yaratmak istediðiniz site adresini giriniz.',
+    'enter email'   => 'Lütfen baðlantýsýný yaratmak istediðiniz E-Posta adresini giriniz.',
+    'enter url description' => 'Lütfen site adresi için açýklama giriniz.',
+    'invalid url'   => 'Site adresi geçersiz. Adres http://, https://, ftp:// ya da mailto: ile baþlamalý.',
+    'enter image url' => 'Resmini eklemek istediðiniz adresi giriniz.',
+    'invalid image url' => 'Resim adresi geçersiz. Adres http://, https:// ya da ftp:// ile baþlamalý.',
+    'enter who you quote' => 'Alýntý yaptýðýnýz kiþinin adýný giriniz. Ýsterseniz bu kýsmý boþ býrakarak geçebilirsiniz.',
+
+    # For the size picker.
+    'x-large'      => 'Extra large font',
+    'large'        => 'Large font',
+    'medium'       => 'Medium font',
+    'small'        => 'Small font',
+    'x-small'      => 'Extra small font',
+
+    # Help titles.
+    'bbcode help'  => 'BBcode Yardým',
+
+    # Color picker tab names and sizes.
+    'rgb'          => 'RGB',
+    'named'        => 'Named colors',
+    'slides'       => 'Slides',
+    'rgb_size'     => '40',
+    'named_size'   => '90',
+    'slides_size'  => '70',
+);
+?>
