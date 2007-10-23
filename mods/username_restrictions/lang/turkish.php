2b24a39f6a55fe25e0f3ff5254cec65da4df9c92
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-10-23 20:54:52 +0000

    # Added Turkish language file.

---
+++ mods/username_restrictions/lang/turkish.php
@@ -0,0 +1,27 @@
+<?php
+
+$PHORUM['DATA']['LANG']['mod_username_restrictions'] = array(
+    'min_length'     => 'Lütfen daha uzun bir kullanýcý adý seçiniz ' .
+                        '(Minimum uzunluk %length% karakterdir).', 
+    'max_length'     => 'Lütfen daha kýsa bir kullanýcý adý seçiniz' .
+                        '(Maksimum uzunluk %length% karakterdir).', 
+    'valid_chars'    => 'Lütfen farklý bir kullanýcý adý seçiniz. ' .
+                        '"%username%" kullanýcý adý bir veya daha fazla geçersiz karakter ' .
+                        'içermektedir. Geçerli karakterler: %valid_chars%. ' .
+                        'Sizin rahatlýðýnýz için, gereksiz karakterler ' .
+                        'aþaðýdaki formda kullanýcý adýnýzdan kaldýrýlmýþtýr.',
+    'allowed_l'      => 'harfler (a-z)',
+    'allowed_n'      => 'sayýlar (0-9)',
+    'allowed_d'      => 'nokta (.)',
+    'allowed_u'      => 'altý çizili (_)',
+    'allowed_h'      => 'tire (-)',
+    'allowed_s'      => 'boþluk ( )',
+    'and'            => 've',
+    'only_lowercase' => 'Lütfen daha farklý bir kullanýcý adý seçiniz. ' .
+                        '"%username%" kullanýcý ismi bir veya daha fazla büyük harf ' .
+                        'içermektedir, fakat sadece küçük karakterlere izin verilmektedir. ' .
+                        'Sizin rahatlýðýnýz için kullanýcý adýnýzdaki büyük karakterler ' .
+                        'aþaðýdaki formda küçültülmüþtür.',
+);
+
+?>
