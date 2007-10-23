828ddcf44afa7d2b2cdcb81cce47bf689883fb5a
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-10-23 12:27:36 +0000

    # Added a script for automatically creating moderator groups for all forums.

---
+++ scripts/create_moderator_groups.php
@@ -0,0 +1,76 @@
+<?php 
+
+// This script creates a moderator group for every forum on the system.
+// After running this script, you can use those groups for assigning
+// moderators to the forums.
+
+// if we are running in the webserver, bail out
+if (isset($_SERVER["REMOTE_ADDR"])) {
+    echo "This script cannot be run from a browser.";
+    return;
+}
+
+define("PHORUM_ADMIN", 1);
+define('phorum_page', 'create_moderator_groups');
+
+chdir(dirname(__FILE__) . "/..");
+require_once './common.php';
+
+// Make sure that the output is not buffered.
+phorum_ob_clean();
+
+print "\nCreate forum moderator groups ...\n";
+
+$forums = phorum_db_get_forums();
+
+// Find out which forums already have a moderator group available.
+$forum_has_moderator = array();
+$groups=phorum_db_get_groups();
+foreach ($groups as $id => $group) {
+    foreach ($group['permissions'] as $forum_id => $permission) {
+        if ($permission & PHORUM_USER_ALLOW_MODERATE_MESSAGES) {
+            $forum_has_moderator[$forum_id] = TRUE;
+        }
+    }
+}
+print_r($forum_has_moderator);
+
+foreach ($forums as $forum_id => $fdata)
+{
+    if (!empty($fdata['folder_flag'])) continue;
+
+    print "> forum {$fdata['name']} ";
+
+    if (!empty($forum_has_moderator[$forum_id])) {
+        print "[USE EXISTING]\n";
+        continue;
+    }
+
+    $path = $fdata['forum_path'];
+    if (!is_array($path)) $path = unserialize($path);
+    array_pop($path);
+    $name = implode('::',array_reverse($path));
+
+    $group_id = phorum_db_add_group("Moderate $name");
+    if (!$group_id) die("Error adding group \"$name\".\n");
+
+    phorum_db_update_group(array(
+        'group_id' => $group_id,
+        'open'     => 0,
+        'permissions' => array(
+            $forum_id => PHORUM_USER_ALLOW_READ |
+                         PHORUM_USER_ALLOW_REPLY |
+                         PHORUM_USER_ALLOW_NEW_TOPIC |
+                         PHORUM_USER_ALLOW_EDIT |
+                         PHORUM_USER_ALLOW_ATTACH |
+                         PHORUM_USER_ALLOW_MODERATE_MESSAGES |
+                         PHORUM_USER_ALLOW_MODERATE_USERS
+        )
+    ));
+     
+    print "[CREATED GROUP]\n";
+}
+
+print "\n\n";
+
+?>
