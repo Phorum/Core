3aadf6038b9820d5159d52bcfdeea9d8763f3513
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-10-23 20:51:37 +0000

    # Added Turkish language file.

---
+++ mods/smileys/lang/turkish.php
@@ -0,0 +1,10 @@
+<?php
+$PHORUM["DATA"]["LANG"]["mod_smileys"] = array(
+    'smiley'        => 'Smiley Ekle',
+    'subjectsmiley' => 'Konu baþlýðýna smiley ekle',
+    'smileys help'  => 'Smiley Yardým',
+
+    # Text for the smileys disable option in the posting editor.
+    'disable'      => 'Mesajýmda Smiley kullanma'
+);
+?>
