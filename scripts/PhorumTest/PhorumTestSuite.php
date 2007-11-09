bfdebc3703230a8e7ee07d60983e69f0901d6004
ts77 <ts77@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-11-09 19:08:14 +0000

    # moved test-framework to phpunit to avoid the need for spikephpcoverage and simpletest in tree - and having more possibilities

---
+++ scripts/PhorumTest/PhorumTestSuite.php
@@ -0,0 +1,338 @@
+<?php
+define('phorum_page','phpunittest');
+
+/*
+ * Prerequisites:
+ * install PHPUnit >= 3.1.x
+ * install PHP >= 5.2.x
+ * install Phorum >= 5.2.x as usual with going through full installation
+ * (optional) install xdebug extension for php (for codecoverage report)
+ * (optional) install graphviz (for codecoverage report)
+ * 
+ * run with:
+ * phpunit PhorumTestSuite
+ * 
+ * or for html/coverage reports:
+ * phpunit --report ./report PhorumTestSuite
+ * (this needs graphviz and xdebug installed!)
+ * (report is put into report-directory in the phorum-directory then!)
+ * 
+ */
+
+require_once 'PHPUnit/Framework.php';
+
+$cwd = getcwd();
+chdir('../..');
+include './common.php';
+
+// need this here to not clutter output with cookie warnings.
+ob_start();
+
+$PHORUM['cache_users'] = 0;
+$PHORUM['cache_messages'] = 0;
+$PHORUM['cache_newflags'] = 0;
+$PHORUM['track_user_activity'] = 1;
+
+//chdir($cwd);
+class PhorumTestSuite extends PHPUnit_Framework_TestSuite
+{
+    public static function suite()
+    {
+        return new PhorumTestSuite('PhorumTest');
+    }
+    
+    protected function setUp() {
+        // to have the same variable everywhere
+        $this->sharedFixture = mt_rand();
+    }
+}
+
+class PhorumTest extends PHPUnit_Framework_TestCase
+{
+        
+        function testPhorumConnect() {
+            $ret = phorum_db_check_connection();        
+            $this->assertTrue($ret);
+        }
+        
+        function testPhorumInstall() {
+            // @TODO first needs some install functions to run
+        }
+        
+        function testPhorumSettingsLoad() {
+            global $PHORUM;
+            phorum_db_load_settings();
+            $this->assertTrue(!empty($PHORUM['internal_version']));
+        }
+        
+        function testPhorumDbForum() {
+            global $PHORUM;
+            
+            // trying to add a forum first
+            $forum = array(
+            'name'=>'PhorumTest Forum',
+            'active'=>1,
+            'parent_id'=>0,
+            'description'=>'PhorumTest forum'
+            );
+
+            $forum_id = phorum_db_add_forum($forum);    
+            $this->assertGreaterThan(0,$forum_id,"Forum added call returned");   
+            
+            // retrieving this forum again
+            $gotforum = phorum_db_get_forums($forum_id);
+            
+            $this->assertGreaterThan(0,count($gotforum),"Got something back");
+            $this->assertTrue(isset($gotforum[$forum_id]),"Forum really added");
+            
+            // checking the retrieved forum against the original one
+            $checkforum = true;
+            foreach($forum as $key => $value) {
+                
+                if($gotforum[$forum_id][$key] != $value) {
+                    $checkforum = false;
+                }
+                
+            }
+            
+            $this->assertTrue($checkforum,'Comparing added Forum');
+            
+            // trying to change that forum
+            $forum_update = array(
+            'forum_id' => $forum_id,
+            'name'=>'PhorumTest Forum - Test2',
+            'active'=>0,
+            'parent_id'=>1,
+            'description'=>'PhorumTest forum - Test2'
+            );            
+            
+            $res = phorum_db_update_forum($forum_update);    
+            $this->assertTrue($res, "Updating Forum");   
+            
+            // retrieving this forum again
+            $gotforum = phorum_db_get_forums($forum_id);
+            
+            $this->assertGreaterThan(0,count($gotforum),"Got the updated Forum");
+            
+            // checking the retrieved forum against the original one
+            $checkforum = true;
+            foreach($forum_update as $key => $value) {
+                
+                if($gotforum[$forum_id][$key] != $value) {
+                    $checkforum = false;
+                }
+                
+            }
+            
+            $this->assertTrue($checkforum,'Comparing updated Forum');
+            
+            // deleting a forum
+            $res=phorum_db_drop_forum($forum_id);
+            $this->assertNull($res, "Deleting Forum");   
+            
+            // retrieving this forum again
+            $gotforum = phorum_db_get_forums($forum_id);
+            
+            $this->assertEquals(0,count($gotforum),"Trying to get the deleted Forum");            
+            
+        }
+        
+        function testUserApiAdd() {
+                        
+            $user = array('password'=>'testPwd',
+                          'active'=>PHORUM_USER_ACTIVE
+                          );
+                          
+            try {
+                $user_id=phorum_api_user_save($user);
+                $this->fail('Adding user (missing username, email, user_id)');
+            }
+            catch (PHPUnit_Framework_Error $expected) {
+                $this->assertTrue(true,'Adding user (username, missing email, user_id)');
+            }
+            
+
+            $user['username']='testuser'.$this->sharedFixture;
+
+            try {
+                $user_id=phorum_api_user_save($user);
+                $this->fail('Adding user (missing email, user_id)');
+            }
+            catch (PHPUnit_Framework_Error $expected) {
+                $this->assertTrue(true,'Adding user (missing email, user_id)');
+            }
+
+            $user['email']='testEmail'.$this->sharedFixture.'@example.com';
+            
+            try {
+                 $user_id=phorum_api_user_save($user);
+                 $this->fail('Adding user (user_id)');
+            
+            } catch (PHPUnit_Framework_Error $expected) {
+                $this->assertTrue(true,'Adding user (missing user_id)');
+            }
+            
+            $user['user_id'] = NULL;
+            
+            $user_id=phorum_api_user_save($user);
+            
+            $this->assertGreaterThan(0,$user_id, "Adding user.");
+            
+        }
+        
+        function testUserApiGet() {
+            
+
+            $user_id = phorum_api_user_search('username','testuser'.$this->sharedFixture,'=');
+                        
+            $gotten_user = phorum_api_user_get($user_id,true);
+                       
+            $this->assertTrue((is_array($gotten_user) && count($gotten_user)),'Retrieve User');
+
+        }
+        
+        function testUserApiSave() {
+            
+            $user_id = phorum_api_user_search('username','testuser'.$this->sharedFixture,'=');
+            
+            $gotten_user = phorum_api_user_get($user_id,true);
+            
+            // now for saving the user
+            $gotten_user['real_name'] = 'foo';
+            
+            $ret = phorum_api_user_save($gotten_user);
+            
+            $this->assertGreaterThan(0,$ret, 'Saved changed user.');
+            
+            $mod_user2 = array('user_id' => $gotten_user['user_id'],'real_name'=>'test');
+            
+            // and saving it raw too
+            $ret = phorum_api_user_save_raw($mod_user2);
+            
+            $this->assertTrue($ret, 'Saved changed user (raw).');
+            
+        }
+        
+        function testUserApiSettings() {
+            
+            
+            // now handling user-settings
+            $user_id = phorum_api_user_search('username','testuser'.$this->sharedFixture,'=');
+                        
+            $ret = phorum_api_user_save_settings(array());
+            
+            $this->assertNull($ret, 'Saving user-settings (no user_id).');
+            
+            $GLOBALS['PHORUM']['user']['user_id']=$user_id;
+            
+            $ret = phorum_api_user_save_settings(array());
+            
+            $this->assertTrue($ret, 'Saving user-settings (empty settings).');
+            
+            $ret = phorum_api_user_save_settings(array('foo'=>'bar'));
+            
+            $this->assertTrue($ret, 'Saving user-settings.');
+
+            // getting settings
+            $ret = phorum_api_user_get_setting('foo');
+            
+            $this->assertEquals($ret,'bar','Getting user-settings.');
+            
+            $ret = phorum_api_user_get_setting('bar');
+            
+            $this->assertNull($ret,'Getting user-settings (unknown key).');    
+
+        }
+        
+        
+        function testUserApiAuthentication() {
+            
+            // authentication
+            $username = 'testuser'.$this->sharedFixture;
+                        
+            $ret = phorum_api_user_authenticate(PHORUM_FORUM_SESSION,$username,'');
+            $this->assertFalse($ret,'User authenticated without password.');
+            
+            $ret = phorum_api_user_authenticate(PHORUM_FORUM_SESSION,$username,'FOO');
+            $this->assertFalse($ret,'User authenticated with wrong password.');
+            
+            $ret = phorum_api_user_authenticate(PHORUM_FORUM_SESSION,$username,'testPwd');
+            $this->assertGreaterThan(0,$ret,'User authenticated with correct password.'); 
+            
+            
+        }
+        
+        function testUserApiDisplayName() {
+            
+            $user_id = phorum_api_user_search('username','testuser'.$this->sharedFixture,'=');
+            
+            // display-name handling
+            $ret = phorum_api_user_get_display_name($user_id,NULL,PHORUM_FLAG_HTML);
+            
+            $this->assertType('string',$ret,'Getting displayname for user (HTML).');
+            
+            $ret = phorum_api_user_get_display_name($user_id,NULL,PHORUM_FLAG_PLAINTEXT);
+            
+            $this->assertType('string',$ret,'Getting displayname for user (Plaintext).');
+
+        }
+        
+        function testUserApiPostcount() {
+            
+            $user_id = phorum_api_user_search('username','testuser'.$this->sharedFixture,'=');
+
+            // incrementing post-count
+            $ret = phorum_api_user_increment_posts($user_id);
+            
+            $this->assertTrue($ret,'Incrementing post count for user.');
+            
+            $user_get = phorum_api_user_get($user_id);
+                        
+            $this->assertTrue($user_get['posts'] == 1,'Checking post count.'); 
+
+        }
+        
+        function testUserApiSetActiveUser() {
+            
+            $user_id = phorum_api_user_search('username','testuser'.$this->sharedFixture,'=');
+            
+            // set active user
+            $GLOBALS['PHORUM']['user'] = phorum_api_user_get($user_id);
+            
+            $ret = phorum_api_user_set_active_user(PHORUM_FORUM_SESSION,$user_id);
+            
+            $this->assertTrue($ret,'Setting user active again.');    
+
+            // create session
+            $ret = phorum_api_user_session_create(PHORUM_FORUM_SESSION);
+            
+            $this->assertTrue($ret,'Creating user-session');
+        }
+        
+        function testUserApiSearch() {
+            $ret = phorum_api_user_search('username','test%','LIKE');
+            $this->assertGreaterThan(0,$ret,'User search.');
+        }
+        
+        function testUserApiUserList() {
+            $ret = phorum_api_user_list();
+            
+            $this->assertGreaterThan(0,count($ret),'Getting User-List.');
+        }
+    
+        // very last one
+        function testUserApiDelete() {
+            
+            $ret = phorum_api_user_delete($this->user_id_used);
+            $this->assertTrue($ret,'User delete.');
+            
+            
+            
+            $ret = phorum_api_user_get($this->user_id_used);
+            
+            $this->assertNull($ret,'Checking for deleted user.');
+            
+            
+        }
+    }
+?>
--- scripts/PhorumTest/readme.txt
+++ scripts/PhorumTest/readme.txt
@@ -2,10 +2,20 @@ This is a start of first UnitTests for Phorum.
 Actually they are more a test on itself with some functions
 to see and check the output.
 
-PhorumTest.php is plain testing and returns a html-site
-PhorumTestCoverage.php does the testing and returns a html-site 
-                   and generates a coverage report in the report-directory.
-                   That takes quite a while and needs the XDebug-PHP-Extension to be installed.
-
-To run these tests / analysis with your webbrowser you need to rename htaccess to .htaccess so that 
-it overrides the one in scripts itself. Be aware to don't keep that open to the public.
+/*
+ * Prerequisites:
+ * install PHPUnit >= 3.1.x
+ * install PHP >= 5.2.x
+ * install Phorum >= 5.2.x as usual with going through full installation
+ * (optional) install xdebug extension for php (for codecoverage report)
+ * (optional) install graphviz (for codecoverage report)
+ * 
+ * run with:
+ * phpunit PhorumTestSuite
+ * 
+ * or for html/coverage reports:
+ * phpunit --report ./report PhorumTestSuite
+ * (this needs graphviz and xdebug installed!)
+ * (report is put into report-directory in the phorum-directory then!)
+ * 
+ */
