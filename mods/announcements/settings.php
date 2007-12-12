06637ba8fceafe11a72ed374e14e3556702acc6c
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-12-12 23:14:47 +0000

    Added an option "Disable automatic displaying of the announcements?" to the announcement module. With that option enabled, the admin can determine for himself at what exact spot in the templates the announcements are visible by adding the {MOD_ANNOUNCEMENTS} template code to them.

--- mods/announcements/settings.php
+++ mods/announcements/settings.php
@@ -1,29 +1,27 @@
 <?php
 
-if(!defined("PHORUM_ADMIN")) return;
+if (!defined("PHORUM_ADMIN")) return;
 
 $error="";
 
-if(count($_POST)){
-
+if (count($_POST))
+{
     $_POST["number_to_show"] = (int)$_POST["number_to_show"];
-    if((int)$_POST["number_to_show"]==0){
-        // sanity check
+    // Sanity check.
+    if ((int)$_POST["number_to_show"] == 0) {
         $_POST["number_to_show"] = 5;
     }
-    
-    if(isset($_POST['vroot_forum_id']) && is_array($_POST['vroot_forum_id'])) {
-    	
-    	foreach($_POST['vroot_forum_id'] as $vroot_id => $vroot_forum_id) {
 
-    		$PHORUM["mod_announcements"]["vroot"][$vroot_id]=(int)$vroot_forum_id;
-    		
-    	}
+    if(isset($_POST['vroot_forum_id']) && is_array($_POST['vroot_forum_id'])) {
+        foreach($_POST['vroot_forum_id'] as $vroot_id => $vroot_forum_id) {
+            $PHORUM["mod_announcements"]["vroot"][$vroot_id] = (int)$vroot_forum_id;
+        }
     }
 
     $PHORUM["mod_announcements"]["forum_id"] = (int)$_POST["forum_id"];
     $PHORUM["mod_announcements"]["pages"] = $_POST["pages"];
-    $PHORUM["mod_announcements"]["only_show_unread"] = $_POST["only_show_unread"];
+    $PHORUM["mod_announcements"]["disable_autodisplay"] = isset($_POST["disable_autodisplay"]) ? 1 : 0;
+    $PHORUM["mod_announcements"]["only_show_unread"] = isset($_POST["only_show_unread"]) ? 1 : 0;
     $PHORUM["mod_announcements"]["number_to_show"] = (int)$_POST["number_to_show"];
     $PHORUM["mod_announcements"]["days_to_show"] = (int)$_POST["days_to_show"];
 
@@ -50,7 +48,10 @@ $page_list = $frm->checkbox("pages[index]", 1, "Forum List (index.php)", $PHORUM
 
 $frm->addrow("Announcements Appear On", $page_list);
 
-$frm->addrow("Show Only Unread?", $frm->checkbox("only_show_unread", 1, "", $PHORUM["mod_announcements"]["only_show_unread"]));
+$row = $frm->addrow("Disable automatic displaying of the announcements?", $frm->checkbox("disable_autodisplay", 1, "Disable automatic displaying", $PHORUM["mod_announcements"]["disable_autodisplay"]));
+$frm->addhelp($row, "Disable automatic displaying", "By default, the announcements will be displayed after the page header automatically. If you need more control over the exact placement, then enable this option. Then you can specify where you want the announcements to appear yourself by placing the code {MOD_ANNOUNCEMENTS} in your templates.");
+
+$frm->addrow("Show only unread messages to the user?", $frm->checkbox("only_show_unread", 1, "Show only unread", $PHORUM["mod_announcements"]["only_show_unread"]));
 $frm->addrow("Number To Show", $frm->text_box("number_to_show", $PHORUM["mod_announcements"]["number_to_show"], 10));
 $frm->addrow("Maximum Days To Show", $frm->text_box("days_to_show", $PHORUM["mod_announcements"]["days_to_show"], 10) . " (0 = forever)");
 
@@ -63,20 +64,20 @@ $vroot_folders = phorum_get_forum_info(3,-1);
 
 if(count($vroot_folders)) {
 	$frm->addbreak("Announcement Forums for Virtual Root Folders");
-	
+
 	foreach($vroot_folders as $vroot_folder_id => $vroot_path) {
-		
+
 		$forum_list_vroot = phorum_get_forum_info(1,$vroot_folder_id);
-		
+
 		$forum_list_vroot[0]='No Announcements for this Virtual Root';
-		
+
 		asort($forum_list_vroot);
-		
+
 		$frm->addrow('VROOT "'.$vroot_path.'"', $frm->select_tag("vroot_forum_id[$vroot_folder_id]", $forum_list_vroot, (isset($PHORUM["mod_announcements"]["vroot"][$vroot_folder_id])?$PHORUM["mod_announcements"]["vroot"][$vroot_folder_id]:0)));
-		
+
 	}
-	
-	
+
+
 }
 
 $frm->show();
