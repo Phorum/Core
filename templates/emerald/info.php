091d44dbb90332eb72783cf4f72e250d061fd433
ts77 <ts77@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-12-15 17:48:58 +0000

    # give it back its real name

--- templates/emerald/info.php
+++ templates/emerald/info.php
@@ -2,7 +2,7 @@
 
     if(!defined("PHORUM")) return;
 
-    $name="Gitaar.net Template";
+    $name="Phorum Emerald Template";
     $version="1.0";
     // uncomment this to hide this template from the user-select-box
     //$template_hide=1;
