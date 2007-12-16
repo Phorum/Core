aaf0cb20bfa4e5385ddf7a7583e29341c74f9f57
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-12-16 11:28:18 +0000

    # Fixed undefined index warning for new forum_path based breadcrumbs.

--- common.php
+++ common.php
@@ -597,13 +597,12 @@ if ( !defined( "PHORUM_ADMIN" ) ) {
         }
     }
 
-    if(!empty($PHORUM['forum_path']) && !is_array($PHORUM['forum_path'])) 
-        $PHORUM['forum_path'] = unserialize($PHORUM['forum_path']);    
-    
+    if(!empty($PHORUM['forum_path']) && !is_array($PHORUM['forum_path']))
+        $PHORUM['forum_path'] = unserialize($PHORUM['forum_path']);
+
     $PHORUM['DATA']['BREADCRUMBS']=array();
 
-
-    if(count($PHORUM['forum_path'])) {
+    if(!empty($PHORUM['forum_path'])) {
         $list_page_url_template = phorum_get_url(PHORUM_LIST_URL, '%forum_id%');
 
         foreach ($PHORUM['forum_path'] as $id => $name) {
