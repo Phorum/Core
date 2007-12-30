59a35e7651213bf7d9706e85201e07c2469f31f3
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2007-12-30 22:46:36 +0000

    # The old user custom fields variable does no longer exist in the code.

--- include/db/upgrade/mysql/2004112601.php
+++ include/db/upgrade/mysql/2004112601.php
@@ -1,9 +1,11 @@
 <?php
 if(!defined("PHORUM_ADMIN")) return;
 
-$upgrade_queries[]="CREATE TABLE {$PHORUM['user_custom_fields_table']} (
+$upgrade_queries[]="
+    CREATE TABLE {$PHORUM['DBCONFIG']['table_prefix']}_user_custom_fields (
         user_id INT DEFAULT '0' NOT NULL ,
         type INT DEFAULT '0' NOT NULL ,
         data TEXT NOT NULL ,
-        PRIMARY KEY ( user_id , type )) TYPE=MyISAM";
+        PRIMARY KEY ( user_id , type )
+    )";
 ?>
