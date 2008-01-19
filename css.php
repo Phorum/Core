98dc3c041625b777cff525eb31c04a9bd17b5dac
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2008-01-19 10:34:55 +0000

    # Provided exit statements after trigger_error() calls for cases where # an error handler is setup that does not terminate the script on its own.

--- css.php
+++ css.php
@@ -154,22 +154,34 @@ $cache_key = $PHORUM['template'] .'|'.
              $settings_t;
 foreach ($module_registrations as $id => $r)
 {
-    if (!isset($r['module'])) trigger_error(
-        "css_register hook: module registration error: " .
-        "the \"module\" field was not set."
-    );
-    if (!isset($r['source'])) trigger_error(
-        "css_register hook: module registration error: " .
-        "the \"source\" field was not set."
-    );
-    if (!isset($r['where'])) trigger_error(
-        "css_register hook: module registration error: " .
-        "the \"where\" field was not set."
-    );
-    if ($r['where'] != 'before' && $r['where'] != 'after') trigger_error(
-        "css_register hook: module registration error: " .
-        "illegal \"where\" field value\"{$r['where']}\"."
-    );
+    if (!isset($r['module'])) {
+        trigger_error(
+            "css_register hook: module registration error: " .
+            "the \"module\" field was not set."
+        );
+        exit(1);
+    }
+    if (!isset($r['source'])) {
+        trigger_error(
+            "css_register hook: module registration error: " .
+            "the \"source\" field was not set."
+        );
+        exit(1);
+    }
+    if (!isset($r['where'])) {
+        trigger_error(
+            "css_register hook: module registration error: " .
+            "the \"where\" field was not set."
+        );
+        exit(1);
+    }
+    if ($r['where'] != 'before' && $r['where'] != 'after') {
+        trigger_error(
+            "css_register hook: module registration error: " .
+            "illegal \"where\" field value\"{$r['where']}\"."
+        );
+        exit(1);
+    }
     if (preg_match('/^(file|template|function)\((.+)\)$/', $r['source'], $m))
     {
         $module_registrations[$id]['type']   = $m[1];
@@ -208,19 +220,25 @@ foreach ($module_registrations as $id => $r)
 
             case "function":
 
-                if (!isset($r['cache_key'])) trigger_error(
-                    "css_register hook: module registration error: " .
-                    "\"cache_key\" field missing for source " .
-                    "\"{$r['source']}\" in module \"{$r['module']}\"."
-                );
+                if (!isset($r['cache_key'])) {
+                    trigger_error(
+                        "css_register hook: module registration error: " .
+                        "\"cache_key\" field missing for source " .
+                        "\"{$r['source']}\" in module \"{$r['module']}\"."
+                    );
+                    exit(1);
+                }
 
                 break;
         }
-    } else trigger_error(
-        "css_register hook: module registration error: " .
-        "illegal format for source definition \"{$r['source']}\" " .
-        "in module \"{$r['module']}\"."
-    );
+    } else {
+        trigger_error(
+            "css_register hook: module registration error: " .
+            "illegal format for source definition \"{$r['source']}\" " .
+            "in module \"{$r['module']}\"."
+        );
+        exit(1);
+    }
 
     $cache_key .= '|' . $r['module'] . ':' . $r['cache_key'];
 }
