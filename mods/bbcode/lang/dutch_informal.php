32b9aa5ce014f8150ef06d6a72b7269441fabb47
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2008-03-02 19:21:53 +0000

    # Added informal Dutch translation for the BBcode module.

---
+++ mods/bbcode/lang/dutch_informal.php
@@ -0,0 +1,55 @@
+<?php
+$PHORUM["DATA"]["LANG"]["mod_bbcode"] = array(
+    # Button descriptions.
+    'bold'          => 'Vet',
+    'italic'        => 'Schuingedrukt',
+    'underline'     => 'Onderstreept',
+    'strike'        => 'Doorgehaald',
+    'size'          => 'Lettergrootte',
+    'subscript'     => 'Onderschrift',
+    'superscript'   => 'Bovenschrift',
+    'color'         => 'Letterkleur',
+    'center'        => 'Tekst centreren',
+    'url'           => 'URL invoegen',
+    'email'         => 'E-mailadres invoegen',
+    'image'         => 'URL van een afbeelding invoegen',
+    'hr'            => 'Horizontale lijn invoegen ',
+    'code'          => 'Opgemaakte code',
+    'quote'         => 'Quote invoegen',
+    'help'          => 'Help',
+
+    # The text that is shown in the quote block.
+    'quote_title'   => 'Quote',
+
+    # Dialogs and errors.
+    'enter size'    => 'Voer de tekstgrootte in.',
+    'enter email'   => 'Voer het emailadres in.',
+    'enter url'     => 'Voer de URL in om naartoe te linken.',
+    'enter url description' => 'Geef een omschrijving voor de link.',
+    'invalid url'   => 'De URL is ongeldig. Deze moet starten met http://, https://, ftp:// of mailto:',
+    'enter image url' => 'Voer de URL in voor de afbeelding die moet worden ingevoegd.',
+    'invalid image url' => 'De URL voor de afbeelding is ongeldig. Deze moet starten met http://, https:// of ftp://',
+    'enter who you quote' => 'Geef de naam op van de te quoten persoon. Indien gewenst, mag dit ook leeg blijven.',
+
+    # For the size picker.
+    'x-large'      => 'Extra groot lettertype',
+    'large'        => 'Groot lettertype ',
+    'medium'       => 'Normaal lettertype',
+    'small'        => 'Klein lettertype',
+    'x-small'      => 'Extra kleinlettertype',
+
+    # Help titles.
+    'bbcode help'  => 'BBcode help',
+
+    # Color picker tab names and sizes.
+    'rgb'          => 'RGB',
+    'named'        => 'Kleurnamen',
+    'slides'       => 'Schuiven',
+    'rgb_size'     => '40',
+    'named_size'   => '80',
+    'slides_size'  => '68',
+
+    # Text for the bbcode disable option in the posting editor.
+    'disable'      => 'BBcode uitschakelen voor dit bericht'
+);
+?>
