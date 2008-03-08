d18584decd70deeb44abbb3df490cb0d81c36751
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2008-03-08 10:42:32 +0000

    # Formatting.

--- mods/announcements/announcements.php
+++ mods/announcements/announcements.php
@@ -32,7 +32,7 @@ function phorum_setup_announcements ()
     // announcement forum.
     if ($PHORUM['vroot'] > 0 && isset($PHORUM["mod_announcements"]["vroot"][$PHORUM['vroot']]) && $PHORUM["forum_id"] != $PHORUM["mod_announcements"]["vroot"][$PHORUM['vroot']]) {
 
-    $ann_forum_id = $PHORUM["mod_announcements"]["vroot"][$PHORUM['vroot']];
+        $ann_forum_id = $PHORUM["mod_announcements"]["vroot"][$PHORUM['vroot']];
 
     // Inside the top level folder, where we have a forum that is configured
     // to be used for announcements and the current forum is not that
