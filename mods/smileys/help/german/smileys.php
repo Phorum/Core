0ca34ec7a0007bad24e5f6d4cc228ced6c46b2a0
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2008-04-29 16:01:13 +0000

    Fixed #738: problems with German help file for smileys module (thanks Oliver!)

--- mods/smileys/help/german/smileys.php
+++ mods/smileys/help/german/smileys.php
@@ -1,38 +1,29 @@
 <?php
-  chdir('../../../../');
-  define('phorum_page','smiley_help');
-  include_once( "./common.php" );
-
   // The definition of the possible uses for a smiley.
   $PHORUM_MOD_SMILEY_USES = array(
-      0   => "N",
-      1   => "T",
-      2   => "NT",
+      0 => 'N',
+      1 => 'B',
+      2 => 'NB'
   );
 ?>
 <html>
   <head>
-    <title>Smiley Hilfe</title>
-    <link rel="stylesheet" type="text/css" href="<?php print $GLOBALS["PHORUM"]["http_path"] ?>/mods/smileys/help/help.css"/>
+    <title>Smiley-Hilfe</title>
+    <link rel="stylesheet" type="text/css" href="<?php print $GLOBALS['PHORUM']['http_path'] ?>/mods/smileys/help/help.css"/>
   </head>
   <body>
-    <h2>Smiley Hilfe</h2>
-
-    In diesem Forum k&ouml;nnen Sie Smileys nutzen. Smileys sind kleine Buchstabengruppen,
-    die als Bilder beim Lesen einer Nachricht angezeigt werden.
-    Meistens dr&uuml;cken diese Stimmungen aus.
-    In der unten stehenden &Uuml;bersicht finden Sie alle verf&uuml;gbaren Smileys.
-    Die "Wo"-Spalte gibt an, wo die Smilies verwendet werden k&ouml;nnen<br />(T = Thema / Betreff, N = Nachricht / Text, NT = beides).
-    <br/><br/>
+    <h2>Smiley-Hilfe</h2>
+    <p>
+      In diesem Forum k&ouml;nnen Sie Smileys nutzen. Smileys sind kleine Buchstabengruppen, die als Bilder beim Lesen einer Nachricht angezeigt werden. Meistens dr&uuml;cken diese Stimmungen aus. In der unten stehenden &Uuml;bersicht finden Sie alle verf&uuml;gbaren Smileys. Die &quot;Wo&quot;-Spalte gibt an, wo Smileys verwendet werden k&ouml;nnen (B=Betreff, N=Nachricht, NB=beide).
+    </p>
     <table cellspacing="1" width="100%">
-    <tr>
-      <th class="PhorumAdminTableHead">Smiley</th>
-      <th class="PhorumAdminTableHead">Bild</th>
-      <th class="PhorumAdminTableHead">Wo</th>
-    </tr>
-    <?php include("./mods/smileys/help/render_smileys_list.php") ?>
+      <tr>
+        <th class="PhorumAdminTableHead">Smiley</th>
+        <th class="PhorumAdminTableHead">Bild</th>
+        <th class="PhorumAdminTableHead">Beschreibung</th>
+        <th class="PhorumAdminTableHead">Wo</th>
+      </tr>
+      <?php include("./mods/smileys/help/render_smileys_list.php") ?>
     </table>
-
-    <br/><br/><br/>
   </body>
 </html>
