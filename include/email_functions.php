f9d2983e31af75891dbf3de0c3743a8164c728f4
ts77 <ts77@3247d6aa-85f2-0310-9c31-93178ea70538>
2008-06-19 19:58:02 +0000

    added localized moderator mails (partly fixing #568)

--- include/email_functions.php
+++ include/email_functions.php
@@ -317,21 +317,25 @@ function phorum_email_moderators($message)
 {
     global $PHORUM;
 
-    $mail_users = phorum_api_user_list_moderators($PHORUM['forum_id'], $PHORUM['email_ignore_admin'], TRUE);
+    $moderators = phorum_api_user_list_moderators($PHORUM['forum_id'], $PHORUM['email_ignore_admin'], TRUE);
 
-    if (count($mail_users)) {
+    $userdata = phorum_api_user_get(array_keys($moderators));
+    
+    $mail_localized_users=array();
+    foreach($userdata as $id => $user) {
+        $lang = empty($user['user_language']) ? $PHORUM['language'] : $user['user_language'];
+        $mail_localized_users[$lang][]=$user['email'];
+    }
+    
+    if (count($mail_localized_users)) {
         include_once("./include/format_functions.php");
+        
         if($message["status"] > 0) { // just notification of a new message
             $mailsubjecttpl = 'NewUnModeratedSubject';
             $mailmessagetpl = 'NewUnModeratedMessage';
-            $mailsubject    = $PHORUM["DATA"]["LANG"]['NewUnModeratedSubject'];
-            $mailmessage    = $PHORUM["DATA"]["LANG"]['NewUnModeratedMessage'];
-
         } else { // posts needing approval
             $mailsubjecttpl = 'NewModeratedSubject';
             $mailmessagetpl = 'NewModeratedMessage';
-            $mailsubject    = $PHORUM["DATA"]["LANG"]['NewModeratedSubject'];
-            $mailmessage    = $PHORUM["DATA"]["LANG"]['NewModeratedMessage'];
         }
 
         $mail_data = array(
@@ -345,20 +349,38 @@ function phorum_email_moderators($message)
             "plain_body"  => phorum_strip_body($message['body']),
             "approve_url" => phorum_get_url(PHORUM_CONTROLCENTER_URL, "panel=messages"),
             "read_url"    => phorum_get_url(PHORUM_READ_URL, $message['thread'], $message['message_id']),
-            "mailmessage" => $mailmessage,
-            "mailsubject" => $mailsubject,
 
             // For email_user_start.
             "mailmessagetpl" => $mailmessagetpl,
             "mailsubjecttpl" => $mailsubjecttpl,
-            "language"    => $PHORUM['language'],
         );
+        
         if (isset($_POST[PHORUM_SESSION_LONG_TERM])) {
             // strip any auth info from the read url
             $mail_data["read_url"] = preg_replace("!,{0,1}" . PHORUM_SESSION_LONG_TERM . "=" . urlencode($_POST[PHORUM_SESSION_LONG_TERM]) . "!", "", $mail_data["read_url"]);
             $mail_data["approve_url"] = preg_replace("!,{0,1}" . PHORUM_SESSION_LONG_TERM . "=" . urlencode($_POST[PHORUM_SESSION_LONG_TERM]) . "!", "", $mail_data["approve_url"]);
         }
-        phorum_email_user($mail_users, $mail_data);
+
+        // go through the user-languages and send mail with their set lang
+        foreach($mail_localized_users as $language => $mail_users)
+        {
+            $language = basename($language);
+
+            if ( file_exists( "./include/lang/$language.php" ) ) {
+                $mail_data['language'] = $language;
+                include( "./include/lang/$language.php" );
+            } else {
+                $mail_data['language'] = $PHORUM['language'];
+                include("./include/lang/{$PHORUM['language']}.php");
+            }
+            $mail_data["mailmessage"] = $PHORUM["DATA"]["LANG"][$mail_data['mailmessagetpl']];
+            $mail_data["mailsubject"] = $PHORUM["DATA"]["LANG"][$mail_data['mailsubjecttpl']];
+            
+            phorum_email_user($mail_users, $mail_data);
+
+        }        
+        
+        
     }
 }
 
