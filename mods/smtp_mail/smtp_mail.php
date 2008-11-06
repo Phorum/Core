97e54b0cde716e874e2422d69d328977cba14f69
ts77 <ts77@3247d6aa-85f2-0310-9c31-93178ea70538>
2008-11-06 23:35:52 +0000

    # smtp mail module ported to a recent phpmailer again, all needed features seem to be supported now and charset/encoding support is just better

--- mods/smtp_mail/smtp_mail.php
+++ mods/smtp_mail/smtp_mail.php
@@ -1,19 +1,17 @@
 <?php
 /*
-* SMTP-Mail-Module v0.8
+* SMTP-Mail-Module
 * made by Thomas Seifert
 * email: thomas (at) phorum.org
 *
 */
 
 if(!defined("PHORUM")) return;
-define('SWIFT_DIRECTORY','./mods/smtp_mail/swiftmailer');
 
 function phorum_smtp_send_messages ($data)
 {
     $PHORUM=$GLOBALS["PHORUM"];
 
-    $from      = $data['from'];
     $addresses = $data['addresses'];
     $subject   = $data['subject'];
     $message   = $data['body'];
@@ -26,21 +24,19 @@ function phorum_smtp_send_messages ($data)
 
         try {
 
-            // include the swiftmailer-class
-
-            require_once SWIFT_DIRECTORY."/Swift.php";
-            require_once SWIFT_DIRECTORY."/Swift/Connection/SMTP.php";
-
-            // set the connection type
-            if($settings['conn'] == 'plain') {
-                $conn_type = Swift_Connection_SMTP::ENC_OFF;
-            } elseif($settings['conn'] == 'ssl') {
-                $conn_type = Swift_Connection_SMTP::ENC_SSL;
-            } elseif($settings['conn'] == 'tls') {
-                $conn_type = Swift_Connection_SMTP::ENC_TLS;
-            } else {
-                $conn_type = Swift_Connection_SMTP::AUTO_DETECT;
-            }
+			require("./mods/smtp_mail/phpmailer/class.phpmailer.php");  
+			  
+			$mail = new PHPMailer();  
+			$mail->PluginDir = "./mods/smtp_mail/phpmailer/";
+			  
+            $mail->CharSet  = $PHORUM["DATA"]["CHARSET"];
+            $mail->Encoding = $PHORUM["DATA"]["MAILENCODING"];
+			$mail->Mailer   = "smtp";  
+			$mail->IsHTML(false);
+			
+		    $mail->From     = $PHORUM['system_email_from_address'];
+		    $mail->Sender   = $PHORUM['system_email_from_address'];
+		    $mail->FromName = $PHORUM['system_email_from_name'];			
 
             if(!isset($settings['host']) || empty($settings['host'])) {
                 $settings['host'] = 'localhost';
@@ -50,49 +46,76 @@ function phorum_smtp_send_messages ($data)
                 $settings['port'] = '25';
             }
 
-            // setup the connection with hostname and port
-            $smtp = new Swift_Connection_SMTP($settings['host'], $settings['port'],$conn_type);
-
+			$mail->Host     = $settings['host'];  
+			$mail->Port     = $settings['port'];
+			
+            // set the connection type
+            if($settings['conn'] == 'ssl') {
+                $mail->SMTPSecure   = "ssl";
+            } elseif($settings['conn'] == 'tls') {
+                $mail->SMTPSecure   = "tls";
+            }
+            
             // smtp-authentication
             if($settings['auth'] && !empty($settings['username'])) {
-                $smtp->setUsername($settings['username']);
-                $smtp->setpassword($settings['password']);
+            	$mail->SMTPAuth=true;
+            	$mail->Username = $settings['username'];
+            	$mail->Password = $settings['password'];
             }
-
-            // construct the swift-mailer
-            $swift = new Swift($smtp);
-
-            // construct the message
-            $message = new Swift_Message($subject, $message, $type="text/plain", $PHORUM["DATA"]["MAILENCODING"], $PHORUM["DATA"]["CHARSET"]);
-
-            $recipients = new Swift_RecipientList();
-
-            if(isset($settings['bcc']) && $settings['bcc'] && $num_addresses > 3){
-
-                $recipients->addTo("undisclosed-recipients:;");
-                $recipients->addBcc($addresses);
-
+            
+            $mail->Body    = $message;
+            $mail->Subject = $subject;
+            
+            // add the newly created message-id
+            $mail->HeaderLine("Message-ID", $data['messageid']);
+            
+            
+            if(!empty($settings['bcc']) && $num_addresses > 3){
+            	$bcc = 1;
+            	$mail->AddAddress("undisclosed-recipients:;");
             } else {
-
-                $recipients->addTo($addresses);
-
+            	$bcc = 0;
+            	// lets keep the connection alive - it could be multiple mails
+            	$mail->SMTPKeepAlive = true;
             }
-
-            $swift->batchSend($message,$recipients,new Swift_Address($PHORUM["system_email_from_address"], $PHORUM["system_email_from_name"]));
-
-        } catch (Swift_Connection_Exception $e) {
-            echo "There was a problem communicating with SMTP: " . $e->getMessage();
-            exit();
-        } catch (Swift_Message_MimeException $e) {
-            echo "There was an unexpected problem building the email:" . $e->getMessage();
-            exit();
-        }
+            
+            foreach ($addresses as $address) {
+            	if($bcc){
+            		$mail->addBCC($address);
+            	} else {
+            		$mail->AddAddress($address);
+            		if(!$mail->Send()) {
+            			echo "There was an error sending the message.\n";
+            			echo "Error returned was ".$mail->ErrorInfo;
+            		}
+   
+				    // Clear all addresses  for next loop  
+					$mail->ClearAddresses(); 
+            	}
+            }
+            
+            // bcc needs just one send call
+            if($bcc) {
+            		if(!$mail->Send()) {
+            			echo "There was an error sending the bcc message.\n";
+            			echo "Error returned was ".$mail->ErrorInfo;
+            		}
+            }
+            
+            // we have to close the connection with pipelining
+            // which is only used in non-bcc mode
+            if(!$bcc) {
+            	$mail->SmtpClose();
+            }
+            
+            
+        } catch (Exception $e) {
+            echo "There was a problem communicating with SMTP: " . $e->getMessage();            exit();
+        } 
     }
 
-    unset($recipients);
     unset($message);
-    unset($swift);
-    unset($smtp);
+    unset($mail);
 
     // make sure that the internal mail-facility doesn't kick in
     return 0;
