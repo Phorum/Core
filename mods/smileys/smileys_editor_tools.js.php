80981693b2e86f289c1b0c76d63b9a0072345701
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2008-11-12 19:26:28 +0000

    Removed content-type header from the smileys javascript code. It is no longer needed now the javascript is incorporated from javascript.php.

--- mods/smileys/smileys_editor_tools.js.php
+++ mods/smileys/smileys_editor_tools.js.php
@@ -1,4 +1,3 @@
-<?php header("Content-Type: text/javascript") ?>
 ///////////////////////////////////////////////////////////////////////////////
 //                                                                           //
 // Copyright (C) 2007  Phorum Development Team                               //
