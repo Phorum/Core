172045937a4a461f5adf896772411776617ea2a8
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2009-02-09 23:44:58 +0000

    # Some more docs added to the js library and restructured the code a bit.

--- include/javascript/phorum-javascript-library.php
+++ include/javascript/phorum-javascript-library.php
@@ -12,6 +12,11 @@ $PJ = jQuery.noConflict();
 
 Phorum = {};
 
+// Phorum.UI is a namespace that is reserved for implementing user
+// interface related functionality.
+
+Phorum.UI = {};
+
 // The version of this lib. This can be used by other code to
 // check if the correct version of the library is loaded.
 // The major number should be incremented in case backward
@@ -20,13 +25,27 @@ Phorum = {};
 
 Phorum.library_version = '1.0';
 
+
 // ----------------------------------------------------------------------
-// Basic console.log debugging support
-//
-// This uses the FireFox console for logging debugging information.
-// When no console is available, then no logging is done at all.
+// console.debug() debugging support
 // ----------------------------------------------------------------------
 
+/**
+ * Use FireFox' console.debug() method for logging debugging information.
+ * When no console is available, then no logging is done at all.
+ *
+ * @param Integer level
+ *     The debug level of the message. Only messages that have a
+ *     debugging level equal to or below the configuration variable
+ *     Phorum.debug.level are logged.
+ *
+ * @param String message
+ *     The message to write to the debug log.
+ *
+ * @param mixed data
+ *     Optional argument containing a variable which' contents should be
+ *     logged. The data will be written to the debug log as a JSON structure.
+ */
 Phorum.debug = function(level, message, data)
 {
     level = parseInt(level);
@@ -45,79 +64,84 @@ Phorum.debug = function(level, message, data)
     }
 }
 
-Phorum.debug.level = 10;
+/**
+ * The debug level. Set this variable to a higher number
+ * for more debugging information.
+ */
+Phorum.debug.level = 0;
+
 
 // ----------------------------------------------------------------------
 // Caching functionality
 //
-// This is a basic caching layer that can be used by Phorum JavaScript
-// code for client side data caching.
+// This is a caching layer that can be used by Phorum JavaScript
+// code for client side data caching. It supports TTLs for automatically
+// expiring the cached data.
 // ----------------------------------------------------------------------
 
-Phorum.Cache = {};
-
-Phorum.Cache.data = {};
-
-// The default TTL for cache entries.
-Phorum.Cache.TTL = 0; 
-
-Phorum.Cache.put = function(cache_id, data, ttl)
+Phorum.Cache =
 {
-    // Determine the TTL to use for the cache entry.
-    if (typeof ttl === 'undefined') {
-        ttl = Phorum.Cache.TTL;
-    } else {
-        ttl = parseInt(ttl);
-        if (ttl == 'NaN') {
+    TTL: 0,   // the default TTL for cache entries
+
+    data: {}, // cache data storage
+
+    put: function(cache_id, data, ttl)
+    {
+        // Determine the TTL to use for the cache entry.
+        if (typeof ttl === 'undefined') {
             ttl = Phorum.Cache.TTL;
+        } else {
+            ttl = parseInt(ttl);
+            if (ttl == 'NaN') {
+                ttl = Phorum.Cache.TTL;
+            }
         }
-    }
 
-    // Determine the expire time. When ttl = 0, then
-    // no expiration is done at all.
-    var expire_t = 0;
-    if (ttl != 0) {
-        var d = new Date();
-        expire_t = d.getTime() + 1000*ttl;
-    }
+        // Determine the expire time. When ttl = 0, then
+        // no expiration is done at all.
+        var expire_t = 0;
+        if (ttl != 0) {
+            var d = new Date();
+            expire_t = d.getTime() + 1000*ttl;
+        }
 
-    // Store the data in the cache.
-    Phorum.Cache.data[cache_id] = [data, expire_t];
-}
+        // Store the data in the cache.
+        Phorum.Cache.data[cache_id] = [data, expire_t];
+    },
 
-Phorum.Cache.get = function(cache_id)
-{
-    // Check if there is a cache entry available.
-    if (typeof Phorum.Cache.data[cache_id] === 'undefined') {
-        Phorum.debug(5, 'Phorum cache miss for cache_id "'+cache_id+'"');
-        return null;
-    }
-    var c = Phorum.Cache.data[cache_id];
-
-    // TTL set? Then check if the cache entry hasn't expired yet.
-    if (c[1] != 0) {
-        var d = new Date();
-        var now = d.getTime();
-        if (now > c[1]) {
-            Phorum.debug(5, 'Phorum cache expired for cache_id "'+cache_id+'"');
+    get: function(cache_id)
+    {
+        // Check if there is a cache entry available.
+        if (typeof Phorum.Cache.data[cache_id] === 'undefined') {
+            Phorum.debug(5, 'Phorum cache miss for cache_id "'+cache_id+'"');
             return null;
         }
+        var c = Phorum.Cache.data[cache_id];
+
+        // TTL set? Then check if the cache entry hasn't expired yet.
+        if (c[1] != 0) {
+            var d = new Date();
+            var now = d.getTime();
+            if (now > c[1]) {
+                Phorum.debug(5, 'Phorum cache expired for cache_id "'+cache_id+'"');
+                return null;
+            }
+        }
+
+        Phorum.debug(5, 'Phorum cache hit for cache_id "'+cache_id+'"');
+        return c[0];
+    },
+
+    // Invalidate a single cache item or the full cache.
+    purge: function(key)
+    {
+        if (typeof key  !== 'undefined') {
+            Phorum.Ajax.cache[key] = null;
+        } else {
+            Phorum.Ajax.cache = {};
+        }
     }
-
-    Phorum.debug(5, 'Phorum cache hit for cache_id "'+cache_id+'"');
-    return c[0];
-}
-
-// Invalidate a single cache item or the full cache.
-Phorum.Cache.purge = function(key)
-{
-    if (typeof key  !== 'undefined') {
-        Phorum.Ajax.cache[key] = null;
-    } else {
-        Phorum.Ajax.cache = {};
-    }
-}
-
+};
 
 // ----------------------------------------------------------------------
 // Ajax communication
@@ -127,115 +151,110 @@ Phorum.Cache.purge = function(key)
 // result to the calling client.
 // ----------------------------------------------------------------------
 
-Phorum.Ajax = {};
-
-// The URL that we can use to access the Phorum Ajax layer script.
-// The 'callback=?' part is a special placeholder for jQuery's JSONP code.
-Phorum.Ajax.URL='<?php print phorum_get_url(PHORUM_AJAX_URL, 'callback=?') ?>';
-
-/**
- * Execute an Ajax Phorum call.
- *
- * @param Object req
- *     The request object. This is object needs at least the property
- *     "call". This req.call property determines what Ajax call has
- *     to be handled by the ajax.php script on the server.
- *     
- */
-Phorum.Ajax.call = function(req)
+Phorum.Ajax =
 {
-    // Check if a call was provided in the request data.
-    if (! req.call) {
-        Phorum.debug(
-            1, 'Phorum.Ajax.call() error: missing property ' +
-            '"call" for the request object', req
-        );
-        if (req.onFailure) req.onFailure(
-            'Phorum.Ajax.call() error: missing property ' +
-            '"call" for the request object.',
-            -1, null
-        );
-        return;
-    }
+    // The URL that we can use to access the Phorum Ajax layer script.
+    // The 'callback=?' part is a special placeholder for jQuery's JSONP code.
+    URL: '<?php print phorum_get_url(PHORUM_AJAX_URL, 'callback=?') ?>',
 
-    // If the req.cache_id property is set for the request, then check
-    // if the data for the request is already available in the
-    // local cache. If yes, then return the data immediately.
-    if (req.cache_id) {
-        var data = Phorum.Cache.get(req.cache_id);
-        if (data != null) {
+    /**
+     * Execute an Ajax Phorum call.
+     *
+     * @param Object req
+     *     The request object. This is object needs at least the property
+     *     "call". This req.call property determines what Ajax call has
+     *     to be handled by the ajax.php script on the server.
+     *
+     *     When the req.cache_id property is set, then the Phorum.Cache is
+     *     used for caching the call result data. It is the task of the
+     *     caller to make sure that the provided cache_id is unique for
+     *     the call that is done.
+     *
+     *     The req.onFailure property can be set for implementing error
+     *     handling. Its value should be a function that handles the error.
+     *     The function will be called with the error message as its argument.
+     *
+     *     The req.onSuccess property can be set for implementing handling for
+     *     a successful Ajax call. Its value should be a function that handles
+     *     the data that was returned by the Ajax call. The function will be
+     *     called with two arguments:
+     *     - the data that was returned by the Ajax call
+     *     - whether (true) or not (false) the result was returned from cache
+     *
+     *     Other properties are sent to the ajax.php script as call arguments.
+     *     What call arguments are available depends on the Phorum Ajax call
+     *     that is called. Check the documentation of the call for details.
+     */
+    call: function(req)
+    {
+        // Check if a call was provided in the request data.
+        if (! req.call) {
             Phorum.debug(
-                4, 'Phorum.Ajax.call calls onSuccess with cached data for ' +
-                'cache_id "'+req.cache_id+'"', data
+                1, 'Phorum.Ajax.call() error: missing property ' +
+                '"call" for the request object', req
             );
-            if (req.onSuccess) {
-                // true = data retrieved from cache.
-                req.onSuccess(data, true);
-            }
+            if (req.onFailure) req.onFailure(
+                'Phorum.Ajax.call() error: missing property ' +
+                '"call" for the request object.',
+                -1, null
+            );
             return;
         }
-    }
 
-    // Create a filtered argument list (without functions and control args).
-    var args = {};
-    for (var key in req) {
-        if (typeof req[key] != 'function' && key != 'cache_id') {
-            // Convert complex arguments to JSON, otherwise they will
-            // not survive the translation to a JSONP request URL.
-            if (typeof(req[key]) == 'object') {
-                args[key] = '$JSON$' + $PJ.toJSON(req[key]);
-            } else {
-                args[key] = req[key];
+        // If the req.cache_id property is set for the request, then check
+        // if the data for the request is already available in the
+        // local cache. If yes, then return the data immediately.
+        if (req.cache_id) {
+            var data = Phorum.Cache.get(req.cache_id);
+            if (data != null) {
+                Phorum.debug(
+                    4, 'Phorum.Ajax.call calls onSuccess with cached data ' +
+                    'for cache_id "'+req.cache_id+'"', data
+                );
+                if (req.onSuccess) {
+                    // true = data retrieved from cache.
+                    req.onSuccess(data, true);
+                }
+                return;
             }
         }
-    }
 
-    // Notify the start of the request loading stage.
-    Phorum.debug(5, 'Phorum.Ajax.call calls server with args', args);
-    if (req.onRequest) req.onRequest(args);
-
-    $PJ.getJSON(Phorum.Ajax.URL, args, function(answer)
-    {
-        Phorum.debug(5, 'Phorum.Ajax.call receives answer from server', answer);
-        if (typeof answer['error'] === 'undefined')
-        {
-            // If the req.cache_id property is set, then we cache the results.
-            if (req.cache_id) Phorum.Cache.put(req.cache_id, answer, 10);
-
-            // false = data not retrieved from cache.
-            Phorum.debug(4, 'Phorum.Ajax.call calls onSuccess with', answer);
-            if (req.onSuccess) req.onSuccess(answer, false);
-        }
-        else
-        {
-            Phorum.debug(4, 'Phorum.Ajax.call calls onFailure with', answer['error']);
-            if (req.onFailure) req.onFailure(answer['error']);
-        }
-    });
-}
-
-// Parse out javascript blocks from the data to eval them. Adding them
-// to the page using innerHTML does not invoke parsing by the browser.
-Phorum.Ajax.evalJavaScript = function(data)
-{
-    var cursor = 0;
-    var start  = 1;
-    var end    = 1;
-
-    while (cursor < data.length && start > 0 && end > 0) {
-        start = data.indexOf('<script', cursor);
-        end   = data.indexOf('</script', cursor);
-        if (end > start && end > -1) {
-            if (start > -1) {
-                var res = data.substring(start, end);
-                start = res.indexOf('>') + 1;
-                res = res.substring(start);
-                if (res.length != 0) {
-                    eval(res);
+        // Create a filtered argument list (without functions and control args).
+        var args = {};
+        for (var key in req) {
+            if (typeof req[key] != 'function' && key != 'cache_id') {
+                // Convert complex arguments to JSON, otherwise they will
+                // not survive the translation to a JSONP request URL.
+                if (typeof(req[key]) == 'object') {
+                    args[key] = '$JSON$' + $PJ.toJSON(req[key]);
+                } else {
+                    args[key] = req[key];
                 }
             }
-            cursor = end + 1;
         }
+
+        // Notify the start of the request loading stage.
+        Phorum.debug(5, 'Phorum.Ajax.call calls server with args', args);
+        if (req.onRequest) req.onRequest(args);
+
+        $PJ.getJSON(Phorum.Ajax.URL, args, function(answer)
+        {
+            Phorum.debug(5, 'Phorum.Ajax.call receives answer from server', answer);
+            if (typeof answer['error'] === 'undefined')
+            {
+                // If the req.cache_id property is set, then we cache the results.
+                if (req.cache_id) Phorum.Cache.put(req.cache_id, answer, 10);
+
+                // false = data not retrieved from cache.
+                Phorum.debug(4, 'Phorum.Ajax.call calls onSuccess with', answer);
+                if (req.onSuccess) req.onSuccess(answer, false);
+            }
+            else
+            {
+                Phorum.debug(4, 'Phorum.Ajax.call calls onFailure with', answer['error']);
+                if (req.onFailure) req.onFailure(answer['error']);
+            }
+        });
     }
-}
+};
 
