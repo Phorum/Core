c8d1768679e699200a7c818f7f09d1774f038ccf
brian <brian@3247d6aa-85f2-0310-9c31-93178ea70538>
2009-02-21 20:54:55 +0000

    # Moved alters into one query for speed.  I tested it at Thomas' request.  He was right

--- include/db/upgrade/mysql-patches/2009021901.php
+++ include/db/upgrade/mysql-patches/2009021901.php
@@ -2,6 +2,16 @@
 
 $upgrade_queries[]=
     "ALTER TABLE {$PHORUM['message_table']}
+        DROP KEY forum_max_message,
+        DROP KEY list_page_flat,
+        DROP KEY list_page_float,
+        DROP KEY new_count,
+        DROP KEY new_threads,
+        DROP KEY next_prev_thread,
+        DROP KEY recent_threads,
+        DROP KEY status_forum,
+        DROP KEY thread_forum,
+        DROP KEY thread_message,
         ADD KEY forum_recent_messages (forum_id,status,datestamp),
         ADD KEY list_page_flat (forum_id,status,parent_id,datestamp),
         ADD KEY list_page_float (forum_id,status,parent_id,modifystamp),
