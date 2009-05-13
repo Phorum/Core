4aa06021ce0118c26d9b07747252be7f49199384
ts77 <ts77@3247d6aa-85f2-0310-9c31-93178ea70538>
2009-05-13 08:03:50 +0000

    MFB: # added setting for mime-magic file and made the fileinfo check useful at all, skipping mime-sniffing check in case the mime-type was verified by the extension.

--- include/api/file.php
+++ include/api/file.php
@@ -805,25 +805,22 @@ function phorum_api_file_retrieve($file, $flags = PHORUM_FLAG_GET)
         $file["file_data"] = base64_decode($dbfile["file_data"]);
     }
 
-    // If the file is not requested for downloading, then check if it is
-    // safe for the browser to view this file. If it is not, then
-    // enable the force download flag to make sure that the browser will
-    // download the file.
-    if (!($flags & PHORUM_FLAG_FORCE_DOWNLOAD))
-    {
-        if (phorum_api_file_browser_sniffs_html($file)) {
-            $flags = $flags | PHORUM_FLAG_FORCE_DOWNLOAD;
-        }
-    }
-
     // Set the MIME type information if it was not set by a module.
     if ($file["mime_type"] === NULL) {
         
+        $extension_mime_type = phorum_api_file_get_mimetype($file["filename"]);
+        
+        // mime magic file in case its needed
+        if(!empty($PHORUM['mime_magic_file'])) {
+            $mime_magic_file = $PHORUM['mime_magic_file']; 
+        } else {
+            $mime_magic_file = NULL;
+        }
         // retrieve the mime-type using the fileinfo extension if its available and enabled
-    	if(function_exists("finfo_open") && 
+        if(function_exists("finfo_open") && 
            (!isset($PHORUM['file_fileinfo_ext']) || !empty($PHORUM['file_fileinfo_ext'])) &&
-           $finfo = @finfo_open(FILEINFO_MIME)) {
-           	
+           $finfo = @finfo_open(FILEINFO_MIME,$mime_magic_file)) {
+            
             $file["mime_type"] = finfo_buffer($finfo,$file['file_data']);
             finfo_close($finfo);
             if ($file["mime_type"] === false) return phorum_api_error_set(
@@ -831,11 +828,31 @@ function phorum_api_file_retrieve($file, $flags = PHORUM_FLAG_GET)
                 "The mime-type of file {$file["file_id"]} couldn't be determined through the" .
                 "fileinfo-extension"
             );
-                
+            // extension mime-type doesn't fit the signature mime-type
+            // make it a download then
+            if($extension_mime_type != $file["mime_type"]) {
+                $flags = $flags | PHORUM_FLAG_FORCE_DOWNLOAD;
+            }
+            $mime_type_verified = true;
         } else {
-            $file["mime_type"] = phorum_api_file_get_mimetype($file["filename"]);
+            $file["mime_type"] = $extension_mime_type;
         }
     }
+    
+    
+
+    // If the file is not requested for downloading, then check if it is
+    // safe for the browser to view this file. If it is not, then
+    // enable the force download flag to make sure that the browser will
+    // download the file.
+    if (!($flags & PHORUM_FLAG_FORCE_DOWNLOAD))
+    {
+        if ($mime_type_verified === false && phorum_api_file_browser_sniffs_html($file)) {
+            $flags = $flags | PHORUM_FLAG_FORCE_DOWNLOAD;
+        }
+    }
+    
+    
     // Allow for post processing on the retrieved file.
     list($file,$flags) = $phorum->modules->hook("file_after_retrieve", array($file,$flags));
 
