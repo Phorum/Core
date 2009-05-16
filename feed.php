9e6c9a6b14692a94c33a9adf12d1d85c58c252f7
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2009-05-16 21:01:03 +0000

    # Implemented the new Feed API in feed.php.

--- feed.php
+++ feed.php
@@ -20,132 +20,45 @@
 define("phorum_page", "feed");
 require_once './common.php';
 
-require_once './include/feed_functions.php';
-
-// somehow we got to a folder
-if(!empty($PHORUM["folder_flag"]) && $PHORUM["forum_id"] != $PHORUM["vroot"]){
-    exit();
+// Find out for what entity / entities we have to create the feed.
+if (empty($PHORUM['forum_id']) || $PHORUM['forum_id'] == $PHORUM['vroot']) {
+    $what  = PHORUM_FEED_VROOT;
+    $which = $PHORUM['vroot'];
+}
+elseif (isset($PHORUM['args'][1])) {
+    $what  = PHORUM_FEED_THREAD;
+    $which = (int) $PHORUM['args'][1];
+}
+elseif (!$PHORUM['folder_flag']) {
+    $what  = PHORUM_FEED_FORUM;
+    $which = $PHORUM['forum_id'];
+}
+else {
+    trigger_error(
+        "The feed script was called with a folder id as the " .
+        "forum_id argument. This is not supported.",
+        E_USER_ERROR
+    );
 }
 
-// get forums this user can read if no forum_id passed
-if(empty($PHORUM["forum_id"])){
-    $forums = $phorum->forums->get();
-} elseif($PHORUM["forum_id"]==$PHORUM["vroot"]){
-    $forums = $phorum->forums->get($PHORUM["forum_id"]);
-} else {
-    // its cheap to copy this even though there is more than needed in it
-    $forums[$PHORUM["forum_id"]] = $PHORUM;
-}
+// The number of items to retrieve.
+// If no amount is provided, then 30 is used by default.
+$count = empty($PHORUM['args']['count']) ? 30 : $PHORUM['args']['count'];
 
-// grab the data from cache if we can
-// only do this with caching enabled
-$cache_key = $_SERVER["REQUEST_URI"].",".$PHORUM["user"]["user_id"];
-if(isset($PHORUM['cache_rss']) && !empty($PHORUM['cache_rss'])) {
-  $cache = phorum_cache_get("feed", $cache_key);
-}
+// Check if reply messages have been requested for the feed.
+$replies = empty($PHORUM["args"]["replies"]) ? 0 : 1;
 
-if(!empty($cache)){
+// Check what output adapter to use for delivering the feed.
+// If no adapter type is provided, then "rss" is used by default.
+$adapter = empty($PHORUM['args']['type']) ? 'rss' : $PHORUM['args']['type'];
 
-    // extract the two members from cache
-    list($data, $content_type) = $cache;
-
-} else {
-
-    // if it wasn't in cache, we need to make it
-
-    // init array
-    $messages = array();
-
-    // check if this is a thread subscription
-    $thread = (isset($PHORUM["args"][1])) ? (int)$PHORUM["args"][1] : 0;
-    if ($thread) $PHORUM["args"]["replies"] = 1;
-
-    // check if we are getting replies
-    $replies = empty($PHORUM["args"]["replies"]) ? false : true;
-
-    // check the feed type
-    $feed_type = empty($PHORUM["args"]["type"]) ? "rss" : $PHORUM["args"]["type"];
-
-    // generate list of forum ids to grab data for
-    $forum_ids = array_keys($forums);
-
-    // get messages
-    $messages = phorum_db_get_recent_messages(30, 0, $forum_ids, $thread, $replies ? LIST_RECENT_MESSAGES : LIST_RECENT_THREADS);
-
-    // remove users from messages array
-    $users = $messages["users"];
-    unset($messages["users"]);
-
-    // run read hooks to get everything formatted
-    if (isset($PHORUM["hooks"]["read"])) {
-        $messages = $phorum->modules->hook("read", $messages);
-    }
-
-    $messages = $phorum->message->format($messages);
-
-    // set up the feed specifics based on the info we are getting
-    if($thread && $PHORUM["forum_id"]){
-
-        // could happen with long threads
-        if(!isset($messages[$thread])) {
-            $thread_start = phorum_db_get_message($thread);
-        } else {
-            $thread_start = $messages[$thread];
-        }
-
-        $feed_url = $phorum->url(PHORUM_FOREIGN_READ_URL, $PHORUM["forum_id"], $thread, $thread);
-        $feed_title = strip_tags($thread_start["subject"]);
-        $feed_description = strip_tags($thread_start["body"]);
-    } elseif($PHORUM["forum_id"]){
-        $feed_url = $phorum->url(PHORUM_LIST_URL);
-        $feed_title = strip_tags($PHORUM["DATA"]["TITLE"]." - ".$PHORUM["DATA"]["NAME"]);
-        $feed_description = strip_tags($PHORUM["DATA"]["DESCRIPTION"]);
-    } else {
-        $feed_url = $phorum->url(PHORUM_INDEX_URL);
-        $feed_title = strip_tags($PHORUM["DATA"]["TITLE"]);
-        $feed_description = (!empty($PHORUM["description"])) ? $PHORUM["description"] : "";
-    }
-
-    // Put the users back in the messages array for the feed functions.
-    $messages["users"] = $users;
-
-    switch($feed_type) {
-
-        case "html":
-            $data = phorum_feed_make_html($messages, $forums, $feed_url, $feed_title, $feed_description);
-            $content_type = "text/html";
-            break;
-
-        case "js":
-            $data = phorum_feed_make_js($messages, $forums, $feed_url, $feed_title, $feed_description);
-            $content_type = "text/javascript";
-            break;
-
-        case "atom":
-            $data = phorum_feed_make_atom($messages, $forums, $feed_url, $feed_title, $feed_description);
-            $content_type = "application/xml";
-            break;
-
-        default:
-            $data = phorum_feed_make_rss($messages, $forums, $feed_url, $feed_title, $feed_description);
-            $content_type = "application/xml";
-            break;
-
-    }
-
-    // stick the xml in cache for 5 minutes for future use
-    if(isset($PHORUM['cache_rss']) && !empty($PHORUM['cache_rss'])) {
-        phorum_cache_put("feed", $cache_key, array($data, $content_type, 600));
-    }
-
-}
-
-// output the proper header and the data
-header("Content-type: $content_type;");
-echo $data;
-
-// Exit here explicitly for not giving back control to portable and
-// embedded Phorum setups.
-exit(0);
+// Generate and send the feed.
+$phorum->feed(
+    $adapter,    // The output adapter to use
+    $what,       // all forums, single forum or single thread
+    $which,      // forum or thread id (not used for "all forums")
+    $count,      // the number of messages to show
+    $replies     // with or without reply messages
+);
 
 ?>
