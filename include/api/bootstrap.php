7ec27ded2310e650fe25d6e91edc99055bc8c6bf
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2009-05-19 23:20:55 +0000

    Added deprecation iformation to the deprecated function names.

--- include/api/bootstrap.php
+++ include/api/bootstrap.php
@@ -402,54 +402,90 @@ function phorum_api_strerror()
 // modules to use the old function calls. These deprecated functions might
 // be removed from future versions of Phorum.
 
+/**
+ * @deprecated Replaced by {@link phorum_api_url()}.
+ */
 function phorum_get_url() {
     Phorum::API()->url; // make sure the URL API layer code is loaded.
     $argv = func_get_args();
     return call_user_func_array('phorum_api_url_get', $argv);
 }
 
+/**
+ * @deprecated Replaced by {@link phorum_api_url_current()}.
+ */
 function phorum_get_current_url($include_query_string = TRUE) {
     return Phorum::API()->url->current($include_query_string);
 }
 
+/**
+ * @deprecated Replaced by {@link phorum_api_url_redirect()}.
+ */
 function phorum_redirect_by_url($url) {
     return Phorum::API()->redirect($url);
 }
 
+/**
+ * @deprecated Replaced by {@link phorum_api_modules_hook()}.
+ */
 function phorum_hook() {
     Phorum::API()->modules; // make sure the Modules API layer code is loaded.
     $argv = func_get_args();
     return call_user_func_array('phorum_api_modules_hook', $argv);
 }
 
+/**
+ * @deprecated Replaced by {@link phorum_api_format_date()}.
+ */
 function phorum_date($picture, $ts) {
     return Phorum::API()->format->date($picture, $ts);
 }
 
+/**
+ * @deprecated Replaced by {@link phorum_api_format_relative_date()}.
+ */
 function phorum_relative_date($ts) {
     return Phorum::API()->format->relative_date($ts);
 }
 
+/**
+ * @deprecated Replaced by {@link phorum_api_format_filesize()}.
+ */
 function phorum_filesize($sz) {
     return Phorum::API()->format->filesize($sz);
 }
 
+/**
+ * @deprecated Replaced by {@link phorum_api_format_strip()}.
+ */
 function phorum_strip_body($body) {
     return Phorum::API()->format->strip($body);
 }
 
+/**
+ * @deprecated Replaced by {@link phorum_api_buffer_clear()}.
+ */
 function phorum_ob_clean() {
     return Phorum::API()->output->clear();
 }
 
+/**
+ * @deprecated Replaced by {@link phorum_api_write_file()}.
+ */
 function phorum_write_file($file, $data) {
     return Phorum::API()->write_file($file, $data);
 }
 
+/**
+ * @deprecated Replaced by {@link phorum_api_format_messages()}.
+ */
 function phorum_format_messages($messages, $author_spec = NULL) {
     return Phorum::API()->message->format($messages, $author_spec);
 }
 
+/**
+ * @deprecated Replaced by {@link phorum_api_mail_check_address()}.
+ */
 function phorum_valid_email($address) {
     return Phorum::API()->mail->check_address($address);
 }
