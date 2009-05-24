26c537bd00d4bb785e9aa058a8a1f66e45c33c81
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2009-05-24 15:31:35 +0000

    # Whoopslet.

--- include/api/custom_field.php
+++ include/api/custom_field.php
@@ -129,7 +129,7 @@ function phorum_api_custom_field_configure($field)
         }
         elseif ($f != 'id' && $field[$f] === NULL) trigger_error(
             "phorum_api_custom_field_configure(): Field $f in " .
-            '\$field parameter cannot be NULL",
+            '$field parameter cannot be NULL',
             E_USER_ERROR
         );
     }
