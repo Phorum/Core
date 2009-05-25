b71e47df49fb63a22bc8de306b76aab53dd7e7ad
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2009-05-25 08:58:28 +0000

    # Implemented Error API call phorum_api_error_backtrace().

--- include/api/error.php
+++ include/api/error.php
@@ -118,4 +118,67 @@ function phorum_api_error_message()
 }
 // }}}
 
+// {{{ Function: phorum_api_error_backtrace()
+/**
+ * Generate a debug backtrace.
+ *
+ * @param integer $skip
+ *     The amount of backtrace levels to skip. The call to this function
+ *     is skipped by default, so you don't have to count that in.
+ *
+ * @param string|NULL $hidepath
+ *     NULL to not hide paths or a string to replace the Phorum path with.
+ *
+ * @return string
+ *     The backtrace in text format.
+ */
+function phorum_api_error_backtrace($skip = 0, $hidepath = "{path to Phorum}")
+{
+    // Allthough Phorum 4.3.0 is the required PHP version
+    // for Phorum at the time of writing, people might still be running
+    // Phorum on older PHP versions. For those people, we'll skip
+    // creation of a backtrace.
+
+    $backtrace = NULL;
+
+    if (function_exists("debug_backtrace"))
+    {
+        $bt = debug_backtrace();
+        $backtrace = '';
+
+        $step_count = 0;
+        foreach ($bt as $id => $step)
+        {
+            // Don't include the call to this function. The $id 1, 2 and 3 are
+            // checked for cases where this API call was called through the
+            // Phorum::API()->error->backtrace() construction.
+            if ($id == 0 ||
+                ($id == 1 && $step['function'] == 'call_user_func_array') ||
+                ($id == 2 && $step['function'] == '__call') ||
+                ($id == 3 && $step['function'] == 'backtrace')) continue;
+
+            // Skip the required number of steps.
+            $step_count ++;
+            if ($step_count <= $skip) continue;
+
+            if ($hidepath !== NULL && isset($step["file"])) {
+                $file = str_replace(PHORUM_PATH, $hidepath, $step["file"]);
+            }
+            $backtrace .= "Function " . $step["function"] . " called" .
+                          (!empty($step["line"])
+                           ? " at\n" .  $file . ":" . $step["line"]
+                           : "") . "\n----\n";
+        }
+    }
+    else
+    {
+        $backtrace = 'PHP function "debug_backtrace" is not available ' .
+                     'on this system. No backtrace could be generated.';
+    }
+
+    return $backtrace;
+}
+
+// }}}
+
 ?>
