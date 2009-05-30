fa62b81ff698ce8026b36f2262b3b0d4f8fb6269
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2009-05-30 11:23:48 +0000

    # Do not show the token timeoutpage in the admin interface in case we do not # see any POST or GET variables besides "module" and/or "phorum_admin_token". # In those cases, there are guaranteed no special acations performed from the # admin interface, meaning that we can safely apply a fresh token and redirect # the user to the requested admin page.

--- include/admin/tokenmissing.php
+++ include/admin/tokenmissing.php
@@ -17,13 +17,9 @@
 //   along with this program.                                                 //
 ////////////////////////////////////////////////////////////////////////////////
 
-    // don't allow this page to be loaded directly
+    // Don't allow this page to be loaded directly.
     if(!defined("PHORUM_ADMIN")) exit();
-    $targetargs = $_SERVER['QUERY_STRING'];
-    $target_html = htmlspecialchars(phorum_admin_build_url($targetargs));
-    $targs_html = htmlspecialchars($targetargs);
-    $post_url = phorum_admin_build_url('base');
-    
+
     if(count($_POST)) {
         if(!empty($_POST['phorum_admin_token']) && 
             $_POST['phorum_admin_token'] == $GLOBALS["PHORUM"]["user"]['settings_data']['admin_token'] &&
@@ -56,18 +52,42 @@
            }
     }
 
-    // update the token and time
-    $GLOBALS["PHORUM"]["user"]['settings_data']['admin_token_time'] = time();
-    $sig_data = $GLOBALS["PHORUM"]["user"]['user_id'].time().$GLOBALS["PHORUM"]["user"]['username'];
-    $GLOBALS["PHORUM"]["user"]['settings_data']['admin_token'] = phorum_generate_data_signature($sig_data);
-
-    $tmp_user = array(
-             	'user_id'=>$GLOBALS["PHORUM"]["user"]['user_id'],
-                'settings_data'=>$GLOBALS["PHORUM"]["user"]['settings_data']
+    // We have no token or our token expired.
+    // Generate a fresh token.
+    $admin_token_time = time();
+    $admin_token = phorum_generate_data_signature(
+        $PHORUM['user']['user_id'].
+        microtime().
+        $PHORUM['user']['username'].
+        $PHORUM['user']['sessid_st']
     );
-    phorum_api_user_save($tmp_user);
-    
-    
+    phorum_api_user_save_settings(array(
+        'admin_token_time' => $admin_token_time,
+        'admin_token'      => $admin_token
+    ));
+    $PHORUM['admin_token'] = $admin_token;
+
+    // If there are no POST or GET variables in the request, besides
+    // "module" and/or "phorum_admin_token", then we can safely load
+    // the requested admin page, without bugging the admin about the
+    // token timeout.
+    $post = $_POST; unset($post['module']); unset($post['phorum_admin_token']);
+    $get  = $_GET;  unset($get['module']);  unset($get['phorum_admin_token']);
+    if (empty($post) && empty($get)) {
+        $module = ''; 
+        if (isset($_POST['module'])) {
+            $module = basename($_POST['module']);
+        } elseif (isset($_GET['module'])) {
+            $module = basename($_GET['module']);
+        }
+        $url = phorum_admin_build_url('module='.urlencode($module));
+        phorum_redirect_by_url($url);
+    }
+
+    $targetargs = $_SERVER['QUERY_STRING'];
+    $target_html = htmlspecialchars(phorum_admin_build_url($targetargs));
+    $targs_html = htmlspecialchars($targetargs);
+    $post_url = phorum_admin_build_url('base');
 ?>
 You are accessing the admin after a security timeout.<br /><br />
 The requested URL was: 
