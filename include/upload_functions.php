a2758b2d380ea6132fce2fdf21e25c865b3daffc
brian <brian@3247d6aa-85f2-0310-9c31-93178ea70538>
2009-06-09 06:22:25 +0000

    Added hook to allow overriding of the maximum upload file size.

--- include/upload_functions.php
+++ include/upload_functions.php
@@ -30,6 +30,8 @@ if ( !defined( "PHORUM" ) ) return;
  */
 function phorum_get_system_max_upload()
 {
+    global $PHORUM;
+
     // Determine limit as imposed by PHP.
     $pms = phorum_phpcfgsize2bytes(ini_get('post_max_size'));
     $umf = phorum_phpcfgsize2bytes(ini_get('upload_max_filesize'));
@@ -37,7 +39,7 @@ function phorum_get_system_max_upload()
 
     // Determines the database server's limit for file uploads. This limit
     // is determined by the maximum packet size that the database can handle.
-    // We asume that there's a 40% overhead in the packet, so that 60% of 
+    // We asume that there's a 40% overhead in the packet, so that 60% of
     // the packet can be used for sending an uploaded file to the database.
     $db_limit = phorum_db_maxpacketsize();
     if ($db_limit != NULL) {
@@ -49,13 +51,54 @@ function phorum_get_system_max_upload()
         $limit = $db_limit;
     }
 
-    return array($limit, $php_limit, $db_limit);
+    $data = array($limit, $php_limit, $db_limit);
+
+    /*
+     * [hook]
+     *     system_max_upload
+     *
+     * [description]
+     *     This hook allows a module to control the maximum file size for
+     *     a file upload.  Most notable would be file system storage.  It
+     *     could ignore the db_limit.
+     *
+     * [category]
+     *     File storage
+     *
+     * [when]
+     *     In
+     *     <filename>include/upload_functions.php</filename>,
+     *     in the function phorum_get_system_max_upload.
+     *
+     * [input]
+     *     An array containing the default limit, the data layer limit and
+     *     the PHP limit
+     *
+     * [output]
+     *     A 3 part array with the limits adjusted as you wish.  The first
+     *     element in the array would be the most important.
+     *
+     * [example]
+     *     <hookcode>
+     *     function phorum_mod_foo_system_max_upload($data)
+     *     {
+     *         // ignore the db_limit
+     *         $data[0] = $data[2];
+     *         return $data;
+     *     }
+     *     </hookcode>
+     */
+    if (isset($PHORUM["hooks"]["system_max_upload"])) {
+        $data = phorum_hook("system_max_upload", $data);
+    }
+
+    return $data;
 }
 
 /**
  * Converts the size parameters that can be used in the PHP ini-file
  * (e.g. 1024, 10k, 8M) to a number of bytes.
- * 
+ *
  * @param The PHP size parameter
  * @return The size parameter, converted to a number of bytes
  */
