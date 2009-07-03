932791fa18b80855211613b475664bab37f48659
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2009-07-03 11:45:23 +0000

    A fix for hosting providers that manage to provide a SCRIPT_URI that does not contain the actually requested HTTP_HOST, probably due to some mass virtual hosting rewrite rules.

--- include/api/url.php
+++ include/api/url.php
@@ -365,8 +365,14 @@ function phorum_api_url_current($include_query_string = TRUE)
 {
     $url = '';
 
-    if (isset($_SERVER['SCRIPT_URI']))
-    {
+    // On some systems, the SCRIPT_URI is set, but using a different host
+    // name than the one in HTTP_HOST (probably due to some mass virtual
+    // hosting request rewriting). If that happens, we do not trust
+    // the SCRIPT_URI. Otherwise, we use the SCRIPT_URI as the current URL.
+    if (isset($_SERVER["SCRIPT_URI"]) && 
+        (!isset($_SERVER['HTTP_HOST']) ||
+         strpos($_SERVER['SCRIPT_URI'], $_SERVER['HTTP_HOST']) !== FALSE)) {
+
         $url = $_SERVER['SCRIPT_URI'];
     }
     else
