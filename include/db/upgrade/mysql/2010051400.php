b87b387da180f8f4fa384520d4f52748fdf0760f
mmakaay <mmakaay@3247d6aa-85f2-0310-9c31-93178ea70538>
2010-10-11 20:40:09 +0000

    # Corrected a wrong table name use in a db upgrade.

--- include/db/upgrade/mysql/2010051400.php
+++ include/db/upgrade/mysql/2010051400.php
@@ -3,7 +3,7 @@ if(!defined("PHORUM_ADMIN")) return;
 
 // Add indexes for improving newflag counting queries on the index.
 $upgrade_queries[] =
-  "ALTER TABLE {$PHORUM['forums_table']} " .
+  "ALTER TABLE {$PHORUM['message_table']} " .
 
   "ADD INDEX forum_thread_count" .
   "(forum_id, parent_id, status, moved, message_id)," .
