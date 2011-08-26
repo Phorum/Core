4b094f07012734a9c658ec7ac851208138dd5f70
Thomas Seifert <thomas.seifert@mysnip.de>
2011-08-26 14:24:58 +0200

    MFB: fixed XSS reported by Russ McRee @ holisticinfosec

--- include/controlcenter/sig.php
+++ include/controlcenter/sig.php
@@ -21,7 +21,6 @@ if (!defined("PHORUM_CONTROL_CENTER")) return;
 
 if(count($_POST)) {
      list($error,$okmsg) = phorum_controlcenter_user_save($panel);
-     $PHORUM["DATA"]["PROFILE"]["signature"] = htmlspecialchars($PHORUM["DATA"]["PROFILE"]["signature"], ENT_COMPAT, $PHORUM["DATA"]["HCHARSET"]);
 }
 
 $PHORUM['DATA']['PROFILE']['SIGSETTINGS'] = 1;
