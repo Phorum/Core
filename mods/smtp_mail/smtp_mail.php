7d13c7f2c47da98ae484420f45385479ea8387a2
oricgn <oliver@riesen.org>
2015-08-19 13:08:20 +0200

    SMTP Module: Upgrade phpmailer
    phpmailer upgraded from version 2.3 to 5.2.10.

--- mods/smtp_mail/smtp_mail.php
+++ mods/smtp_mail/smtp_mail.php
@@ -35,19 +35,19 @@ function phorum_smtp_send_messages ($data)
 
         try {
 
-      require_once("./mods/smtp_mail/phpmailer/class.phpmailer.php");
+            require_once("./mods/smtp_mail/phpmailer/class.phpmailer.php");
 
-      $mail = new PHPMailer();
-      $mail->PluginDir = "./mods/smtp_mail/phpmailer/";
+            $mail = new PHPMailer();
+            $mail->PluginDir = "./mods/smtp_mail/phpmailer/";
 
             $mail->CharSet  = $PHORUM["DATA"]["CHARSET"];
             $mail->Encoding = $PHORUM["DATA"]["MAILENCODING"];
-      $mail->Mailer   = "smtp";
-      $mail->IsHTML(false);
+            $mail->Mailer   = "smtp";
+            $mail->isHTML(false);
 
-        $mail->From     = $PHORUM['system_email_from_address'];
-        $mail->Sender   = $PHORUM['system_email_from_address'];
-        $mail->FromName = $PHORUM['system_email_from_name'];
+            $mail->From     = $PHORUM['system_email_from_address'];
+            $mail->Sender   = $PHORUM['system_email_from_address'];
+            $mail->FromName = $PHORUM['system_email_from_name'];
 
             if(!isset($settings['host']) || empty($settings['host'])) {
                 $settings['host'] = 'localhost';
@@ -57,21 +57,21 @@ function phorum_smtp_send_messages ($data)
                 $settings['port'] = '25';
             }
 
-      $mail->Host     = $settings['host'];
-      $mail->Port     = $settings['port'];
+            $mail->Host     = $settings['host'];
+            $mail->Port     = $settings['port'];
 
             // set the connection type
             if($settings['conn'] == 'ssl') {
-                $mail->SMTPSecure   = "ssl";
+                $mail->SMTPSecure = "ssl";
             } elseif($settings['conn'] == 'tls') {
-                $mail->SMTPSecure   = "tls";
+                $mail->SMTPSecure = "tls";
             }
 
             // smtp-authentication
             if($settings['auth'] && !empty($settings['username'])) {
-              $mail->SMTPAuth=true;
-              $mail->Username = $settings['username'];
-              $mail->Password = $settings['password'];
+                $mail->SMTPAuth = true;
+                $mail->Username = $settings['username'];
+                $mail->Password = $settings['password'];
             }
 
             $mail->Body    = $message;
@@ -79,125 +79,125 @@ function phorum_smtp_send_messages ($data)
 
             // add the newly created message-id
             // in phpmailer as a public var
-            $mail->MessageID=$data['messageid'];
+            $mail->MessageID = $data['messageid'];
 
             // add custom headers if defined
             if(!empty($data['custom_headers'])) {
                 // custom headers in phpmailer are added one by one
-              $custom_headers = explode("\n",$data['custom_headers']);
-              foreach($custom_headers as $cheader) {
-                $mail->AddCustomHeader($cheader);
-              }
+                $custom_headers = explode("\n",$data['custom_headers']);
+                foreach($custom_headers as $cheader) {
+                    $mail->addCustomHeader($cheader);
+                }
             }
 
             // add attachments if provided
             if(isset($data['attachments']) && count($data['attachments'])) {
-              /*
-               * Expected input is an array of
-               *
-               * array(
-               * 'filename'=>'name of the file including extension',
-               * 'filedata'=>'plain (not encoded) content of the file',
-               * 'mimetype'=>'mime type of the file', (optional)
-               * )
-               *
-               */
+                /*
+                 * Expected input is an array of
+                 *
+                 * array(
+                 * 'filename'=>'name of the file including extension',
+                 * 'filedata'=>'plain (not encoded) content of the file',
+                 * 'mimetype'=>'mime type of the file', (optional)
+                 * )
+                 *
+                 */
 
-              foreach($data['attachments'] as $att_id => $attachment) {
-                $att_type = (!empty($attachment['mimetype']))?$attachment['mimetype']:'application/octet-stream';
-                $mail->AddStringAttachment($attachment['filedata'],$attachment['filename'],'base64',$att_type);
+                foreach($data['attachments'] as $att_id => $attachment) {
+                    $att_type = (!empty($attachment['mimetype']))?$attachment['mimetype']:'application/octet-stream';
+                    $mail->addStringAttachment($attachment['filedata'],$attachment['filename'],'base64',$att_type);
 
-                // try to unset it in the original array to save memory
-                unset($data['attachments'][$att_id]);
-              }
+                    // try to unset it in the original array to save memory
+                    unset($data['attachments'][$att_id]);
+                }
 
             }
 
             if(!empty($settings['bcc']) && $num_addresses > 3){
-              $bcc = 1;
-              $mail->AddAddress("undisclosed-recipients:;");
+                $bcc = 1;
+                $mail->addAddress("undisclosed-recipients:;");
             } else {
-              $bcc = 0;
-              // lets keep the connection alive - it could be multiple mails
-              $mail->SMTPKeepAlive = true;
+                $bcc = 0;
+                // lets keep the connection alive - it could be multiple mails
+                $mail->SMTPKeepAlive = true;
             }
 
             foreach ($addresses as $address) {
-              if($bcc){
-                $mail->addBCC($address);
-              } else {
-                $mail->AddAddress($address);
-                if(!$mail->Send()) {
-                    $error_msg  = "There was an error sending the message.";
-                    $detail_msg = "Error returned was: ".$mail->ErrorInfo;
+                if($bcc){
+                    $mail->addBCC($address);
+                } else {
+                    $mail->addAddress($address);
+                    if(!$mail->send()) {
+                        $error_msg  = "There was an error sending the message.";
+                        $detail_msg = "Error returned was: ".$mail->ErrorInfo;
 
                         if (function_exists('event_logging_writelog')) {
                             event_logging_writelog(array(
                                "source"    => "smtp_mail",
                                "message"   => $error_msg,
                                "details"   => $detail_msg,
-                 "loglevel"  => EVENTLOG_LVL_ERROR,
+                               "loglevel"  => EVENTLOG_LVL_ERROR,
                                "category"  => EVENTLOG_CAT_MODULE
                             ));
                         }
                         if(!isset($settings['show_errors']) || !empty($settings['show_errors'])) {
-                        echo $error_msg."\n";
-                        echo $detail_msg;
+                            echo $error_msg."\n";
+                            echo $detail_msg;
                         }
-                } elseif(!empty($settings['log_successful'])) {
-                      if (function_exists('event_logging_writelog')) {
+                    } elseif(!empty($settings['log_successful'])) {
+                          if (function_exists('event_logging_writelog')) {
                             event_logging_writelog(array(
                                "source"    => "smtp_mail",
                                "message"   => "Email successfully sent",
                                "details"   => "An email has been sent:\nTo:$address\nSubject: $subject\nBody: $message\n" ,
-                 "loglevel"  => EVENTLOG_LVL_INFO,
+                               "loglevel"  => EVENTLOG_LVL_INFO,
                                "category"  => EVENTLOG_CAT_MODULE
                           ));
                         }
+                    }
+                    // Clear all addresses  for next loop
+                    $mail->clearAddresses();
                 }
-            // Clear all addresses  for next loop
-          $mail->ClearAddresses();
-              }
             }
 
             // bcc needs just one send call
             if($bcc) {
-                if(!$mail->Send()) {
-                   $error_msg  = "There was an error sending the bcc message.";
-                   $detail_msg = "Error returned was: ".$mail->ErrorInfo;
+                    if(!$mail->send()) {
+                       $error_msg  = "There was an error sending the bcc message.";
+                       $detail_msg = "Error returned was: ".$mail->ErrorInfo;
 
                        if (function_exists('event_logging_writelog')) {
                             event_logging_writelog(array(
                                "source"    => "smtp_mail",
                                "message"   => $error_msg,
                                "details"   => $detail_msg,
-                 "loglevel"  => EVENTLOG_LVL_ERROR,
+                               "loglevel"  => EVENTLOG_LVL_ERROR,
                                "category"  => EVENTLOG_CAT_MODULE
                             ));
                        }
                        if(!isset($settings['show_errors']) || !empty($settings['show_errors'])) {
-                       echo $error_msg."\n";
-                       echo $detail_msg;
+                           echo $error_msg."\n";
+                           echo $detail_msg;
                        }
-                } elseif(!empty($settings['log_successful'])) {
-                      if (function_exists('event_logging_writelog')) {
-                        $address_join = implode(",",$addresses);
+                    } elseif(!empty($settings['log_successful'])) {
+                          if (function_exists('event_logging_writelog')) {
+                            $address_join = implode(",",$addresses);
 
                             event_logging_writelog(array(
                                "source"    => "smtp_mail",
                                "message"   => "BCC-Email successfully sent",
                                "details"   => "An email (bcc-mode) has been sent:\nBCC:$address_join\nSubject: $subject\nBody: $message\n" ,
-                 "loglevel"  => EVENTLOG_LVL_INFO,
+                               "loglevel"  => EVENTLOG_LVL_INFO,
                                "category"  => EVENTLOG_CAT_MODULE
                           ));
                         }
-                }
+                    }
             }
 
             // we have to close the connection with pipelining
             // which is only used in non-bcc mode
             if(!$bcc) {
-              $mail->SmtpClose();
+                $mail->smtpClose();
             }
 
 
@@ -210,7 +210,7 @@ function phorum_smtp_send_messages ($data)
                       "source"    => "smtp_mail",
                       "message"   => $error_msg,
                       "details"   => $detail_msg,
-              "loglevel"  => EVENTLOG_LVL_ERROR,
+                      "loglevel"  => EVENTLOG_LVL_ERROR,
                       "category"  => EVENTLOG_CAT_MODULE
                 ));
             }
