68bcc4232990f0bc6d42bac28a39c15a3067ced4
oricgn <oliver@riesen.org>
2016-07-29 14:22:03 +0200

    New year 2016

--- include/api/diff.php
+++ include/api/diff.php
@@ -1,7 +1,7 @@
 <?php
 ////////////////////////////////////////////////////////////////////////////////
 //                                                                            //
-//   Copyright (C) 2011  Phorum Development Team                              //
+//   Copyright (C) 2016  Phorum Development Team                              //
 //   http://www.phorum.org                                                    //
 //                                                                            //
 //   This program is free software. You can redistribute it and/or modify     //
@@ -36,7 +36,7 @@
  *
  * @package    PhorumAPI
  * @subpackage Diff
- * @copyright  2011, Phorum Development Team
+ * @copyright  2016, Phorum Development Team
  * @license    Phorum License, http://www.phorum.org/license.txt
  */
 
--- include/api/forums.php
+++ include/api/forums.php
@@ -1,7 +1,7 @@
 <?php
 ////////////////////////////////////////////////////////////////////////////////
 //                                                                            //
-//   Copyright (C) 2011  Phorum Development Team                              //
+//   Copyright (C) 2016  Phorum Development Team                              //
 //   http://www.phorum.org                                                    //
 //                                                                            //
 //   This program is free software. You can redistribute it and/or modify     //
@@ -66,7 +66,7 @@
  *
  * @package    PhorumAPI
  * @subpackage Forums
- * @copyright  2011, Phorum Development Team
+ * @copyright  2016, Phorum Development Team
  * @license    Phorum License, http://www.phorum.org/license.txt
  *
  * @todo Implement PHORUM_FLAG_INCLUDE_EMPTY_FOLDERS flag.
