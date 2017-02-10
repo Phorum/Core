b8135ad1204a197fa13004a0bef9047c955c34d9
oricgn <oliver@riesen.org>
2017-02-10 12:45:24 +0100

    New year 2017

--- include/api/base.php
+++ include/api/base.php
@@ -1,7 +1,7 @@
 <?php
 ////////////////////////////////////////////////////////////////////////////////
 //                                                                            //
-//   Copyright (C) 2016  Phorum Development Team                              //
+//   Copyright (C) 2017 Phorum Development Team                               //
 //   http://www.phorum.org                                                    //
 //                                                                            //
 //   This program is free software. You can redistribute it and/or modify     //
@@ -26,7 +26,7 @@
  *
  * @package    PhorumAPI
  * @subpackage BaseAPI
- * @copyright  2016, Phorum Development Team
+ * @copyright  2017, Phorum Development Team
  * @license    Phorum License, http://www.phorum.org/license.txt
  */
 
