dcff712b4765548933c35186fb91b15943c0860d
oricgn <oliver@riesen.org>
2017-09-13 17:38:24 +0200

    Extend PhorumFilesizeTest

--- tests/unit/phorumFileSizeTest.php
+++ tests/unit/phorumFileSizeTest.php
@@ -4,20 +4,23 @@ require_once __DIR__ . '/../../include/format_functions.php';
 
 class formatFunctionsTest extends \PHPUnit_Framework_TestCase
 {
-    public function getFileSizeData()
+    public function getFilesizeData()
     {
         return array(
-            [2 * 1024 * 1024, '2&nbsp;MB'],
+            [1, '1&nbsp;byte'],
+            [3, '3&nbsp;bytes'],
+            [1023, '1023&nbsp;bytes'],
+            [1024, '1&nbsp;KB'],
+            [3 * 1024, '3&nbsp;KB'],
             [1024 * 1024, '1&nbsp;MB'],
+            [2 * 1024 * 1024, '2&nbsp;MB'],
             [3.14 * 1024 * 1024, '3.14&nbsp;MB'],
             [3.145 * 1024 * 1024, '3.15&nbsp;MB'],
-            [3 * 1024, '3&nbsp;KB'],
-            [3, '3&nbsp;bytes'],
         );
     }
 
     /**
-     * @dataProvider getFileSizeData
+     * @dataProvider getFilesizeData
      */
     public function testPhorumFilesize($bytes, $expected)
     {
