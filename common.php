66fd48bfe2cc22f15cb812c85d2aa4562d2b0460
Brian Moon <brian@moonspot.net>
2026-08-13 11:17:54 -0500

    Version changed to 6.0.5

--- common.php
+++ common.php
@@ -85,7 +85,7 @@ function phorum_set_cookie(
 }
 
 // the Phorum version
-define( "PHORUM", "6.0.4" );
+define( "PHORUM", "6.0.5" );
 
 // our database schema version in format of year-month-day-serial
 define( "PHORUM_SCHEMA_VERSION", "2010101500" );
